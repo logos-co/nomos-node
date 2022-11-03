0fcfd92a555e117c9eb1ceae65a14bcc825118cd
Giacomo Pasini <Zeegomo@users.noreply.github.com>
2022-11-03 14:28:37 +0100

    Initial network service (#2)
    * initial network service
    
    * [style] unpack inbound relay

---
+++ nomos-services/src/network/backends/mod.rs
@@ -0,0 +1,12 @@
+use super::*;
+use overwatch::services::state::ServiceState;
+use tokio::sync::broadcast::Receiver;
+
+pub trait NetworkBackend {
+    type Config: Clone + Send + Sync + 'static;
+    type State: ServiceState<Settings = Self::Config> + Clone;;
+
+    fn new(config: Self::Config) -> Self;
+    fn broadcast(&self, msg: NetworkData);
+    fn subscribe(&mut self, event: EventKind) -> Receiver<NetworkEvent>;
+}
