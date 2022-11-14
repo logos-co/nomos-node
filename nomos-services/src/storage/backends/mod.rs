69ac85b5e8bdc4a2080f50f57113cc13f52c98d7
danielsanchezq <sanchez.quiros.daniel@gmail.com>
2022-11-14 13:46:49 +0100

    Pipe skeleton for StorageService

--- nomos-services/src/storage/backends/mod.rs
+++ nomos-services/src/storage/backends/mod.rs
@@ -1,20 +1,18 @@
+pub mod sled;
+
+// std
 use std::error::Error;
+// crates
+use bytes::Bytes;
+// internal
 
 pub trait StorageBackend {
-    type Config: Clone + Send + Sync + 'static;
+    type Settings: Clone + Send + Sync + 'static;
     type Error: Error;
-    type Transaction;
-    fn new(config: Self::Config) -> Self;
-    fn store<K: AsRef<[u8]>, T: Into<Vec<u8>>>(
-        &mut self,
-        key: K,
-        value: T,
-    ) -> Result<(), Self::Error>;
-    fn load<K: AsRef<[u8]>, T: From<Vec<u8>> + Sized>(&mut self, key: &K)
-        -> Result<T, Self::Error>;
-    fn remove<K: AsRef<[u8]>, T: From<Vec<u8>>>(
-        &mut self,
-        key: &K,
-    ) -> Result<Option<T>, Self::Error>;
-    fn execute(&mut self, transaction: Self::Transaction) -> Result<(), Self::Error>;
+    type Transaction: Send + Sync;
+    fn new(config: Self::Settings) -> Self;
+    fn store<K: AsRef<[u8]>, T: Into<Bytes>>(&self, key: K, value: T) -> Result<(), Self::Error>;
+    fn load<K: AsRef<[u8]>, T: From<Bytes> + Sized>(&self, key: &K) -> Result<T, Self::Error>;
+    fn remove<K: AsRef<[u8]>, T: From<Bytes>>(&self, key: &K) -> Result<Option<T>, Self::Error>;
+    fn execute(&self, transaction: Self::Transaction) -> Result<(), Self::Error>;
 }
--- nomos-services/src/storage/mod.rs
+++ nomos-services/src/storage/mod.rs
@@ -1,1 +1,66 @@
-mod backends;
+pub mod backends;
+
+// std
+
+use async_trait::async_trait;
+use std::fmt::{Debug, Formatter};
+// crates
+use bytes::Bytes;
+use overwatch::services::handle::ServiceStateHandle;
+// internal
+use backends::StorageBackend;
+use overwatch::services::relay::RelayMessage;
+use overwatch::services::state::{NoOperator, NoState};
+use overwatch::services::{ServiceCore, ServiceData, ServiceId};
+
+pub enum StorageMsg<Backend: StorageBackend> {
+    Load {
+        key: Box<[u8]>,
+        reply_channel: tokio::sync::oneshot::Sender<Bytes>,
+    },
+    Store {
+        key: Box<[u8]>,
+        value: Bytes,
+    },
+    Remove {
+        key: Box<[u8]>,
+    },
+    Execute {
+        transaction: Backend::Transaction,
+    },
+}
+
+impl<Backend: StorageBackend> Debug for StorageMsg<Backend> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        todo!()
+    }
+}
+
+impl<Backend: StorageBackend + 'static> RelayMessage for StorageMsg<Backend> {}
+
+pub struct StorageService<Backend: StorageBackend + Send + Sync + 'static> {
+    backend: Backend,
+    service_state: ServiceStateHandle<Self>,
+}
+
+#[async_trait]
+impl<Backend: StorageBackend + Send + Sync + 'static> ServiceCore for StorageService<Backend> {
+    fn init(mut service_state: ServiceStateHandle<Self>) -> Self {
+        Self {
+            backend: Backend::new(service_state.settings_reader.get_updated_settings()),
+            service_state,
+        }
+    }
+
+    async fn run(self) {
+        todo!()
+    }
+}
+
+impl<Backend: StorageBackend + Send + Sync> ServiceData for StorageService<Backend> {
+    const SERVICE_ID: ServiceId = "Storage";
+    type Settings = Backend::Settings;
+    type State = NoState<Self::Settings>;
+    type StateOperator = NoOperator<Self::State>;
+    type Message = StorageMsg<Backend>;
+}
