6fc8c95f90b8874ca6c07429dcd0370ed7c4caf7
danielsanchezq <sanchez.quiros.daniel@gmail.com>
2022-11-17 15:30:52 +0100

    Refactor sled to adap to latest StorageBackend traits changes

--- nomos-services/src/storage/backends/sled.rs
+++ nomos-services/src/storage/backends/sled.rs
@@ -1,9 +1,13 @@
+use std::marker::PhantomData;
 // std
 use std::path::PathBuf;
 // crates
+use crate::storage::backends::{StorageSerde, StorageTransaction};
 use async_trait::async_trait;
 use bytes::Bytes;
-use sled::transaction::{ConflictableTransactionResult, TransactionError, TransactionalTree};
+use sled::transaction::{
+    ConflictableTransactionResult, TransactionError, TransactionResult, TransactionalTree,
+};
 // internal
 use super::StorageBackend;
 
@@ -16,38 +20,50 @@ pub type SledTransaction = Box<
     dyn Fn(&TransactionalTree) -> ConflictableTransactionResult<Bytes, sled::Error> + Send + Sync,
 >;
 
+impl StorageTransaction for SledTransaction {
+    type Result = TransactionResult<Bytes, sled::Error>;
+    type Transaction = Self;
+}
+
 /// Sled storage backend
-pub struct SledBackend(sled::Db);
+pub struct SledBackend<SerdeOp> {
+    sled: sled::Db,
+    _serde_op: PhantomData<SerdeOp>,
+}
 
 #[async_trait]
-impl StorageBackend for SledBackend {
+impl<SerdeOp: StorageSerde + Send + Sync + 'static> StorageBackend for SledBackend<SerdeOp> {
     type Settings = SledBackendSettings;
     type Error = TransactionError;
     type Transaction = SledTransaction;
+    type SerdeOperator = SerdeOp;
 
     fn new(config: Self::Settings) -> Self {
-        Self(
-            sled::open(config.db_path)
+        Self {
+            sled: sled::open(config.db_path)
                 // TODO: We should probably make initialization failable
                 .unwrap(),
-        )
+            _serde_op: Default::default(),
+        }
     }
 
     async fn store(&mut self, key: Bytes, value: Bytes) -> Result<(), Self::Error> {
-        let _ = self.0.insert(key, value.to_vec())?;
+        let _ = self.sled.insert(key, value.to_vec())?;
         Ok(())
     }
 
     async fn load(&mut self, key: &[u8]) -> Result<Option<Bytes>, Self::Error> {
-        Ok(self.0.get(key)?.map(|ivec| ivec.to_vec().into()))
+        Ok(self.sled.get(key)?.map(|ivec| ivec.to_vec().into()))
     }
 
     async fn remove(&mut self, key: &[u8]) -> Result<Option<Bytes>, Self::Error> {
-        Ok(self.0.remove(key)?.map(|ivec| ivec.to_vec().into()))
+        Ok(self.sled.remove(key)?.map(|ivec| ivec.to_vec().into()))
     }
 
-    async fn execute(&mut self, transaction: Self::Transaction) -> Result<(), Self::Error> {
-        self.0.transaction(transaction)?;
-        Ok(())
+    async fn execute(
+        &mut self,
+        transaction: Self::Transaction,
+    ) -> Result<<Self::Transaction as StorageTransaction>::Result, Self::Error> {
+        Ok(self.sled.transaction(transaction))
     }
 }
