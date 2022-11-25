f8800dabcf06d677642bc9513e2d819af4f97356
al8n <scygliu1@gmail.com>
2022-11-25 20:42:11 +0800

    metrics service

--- nomos-services/metrics/src/lib.rs
+++ nomos-services/metrics/src/lib.rs
@@ -1,33 +1,34 @@
-use std::{
-    borrow::Cow,
-    collections::HashMap,
-    sync::Arc,
-    time::Duration,
-};
-
-use axum::{Extension, Server, Router, http::{HeaderValue, Method, header::{CONTENT_TYPE, USER_AGENT}}};
-use futures::future::BoxFuture;
-use overwatch_rs::{services::{
+use overwatch_rs::services::{
     handle::ServiceStateHandle,
-    relay::{RelayMessage, InboundRelay},
+    relay::{InboundRelay, RelayMessage},
     state::{NoOperator, NoState},
-    ServiceCore, ServiceData,
-}, overwatch::{handle::OverwatchHandle, commands::{OverwatchCommand, RelayCommand}}};
+    ServiceCore, ServiceData, ServiceId,
+};
 use parking_lot::Mutex;
-use serde::{Serialize, Deserialize};
-use tower_http::{cors::{CorsLayer, Any}, trace::TraceLayer};
+use serde::{de::DeserializeOwned, Serialize};
+use std::{collections::HashMap, sync::Arc};
 
-pub struct Metrics<E> 
-    where E: core::fmt::Debug + Send + Sync + 'static
+pub trait MetricData:
+    core::fmt::Debug
+    + Clone
+    + Serialize
+    + DeserializeOwned
+    + async_graphql::OutputType
+    + Send
+    + Sync
+    + 'static
 {
-    handle: OverwatchHandle,
-    settings: MetricsSettings,
+}
+
+#[repr(transparent)]
+pub struct Metrics<E: MetricData> {
     inbound_relay: InboundRelay<MetricsMessage<E>>,
 }
 
 #[derive(Debug, Clone)]
+#[repr(transparent)]
 struct MetricsBackend<E> {
-    stack: Arc<Mutex<HashMap<Cow<'static, str>, Vec<Data<E>>>>>,
+    stack: Arc<Mutex<HashMap<ServiceId, E>>>,
 }
 
 impl<E> Default for MetricsBackend<E> {
@@ -39,116 +40,45 @@ impl<E> Default for MetricsBackend<E> {
 }
 
 #[async_graphql::Object]
-impl<E> MetricsBackend<E> 
-where E: core::fmt::Debug + Clone + Send + Sync + 'static + async_graphql::OutputType
-{
-    pub async fn load(&self, name: String) -> Option<Vec<Data<E>>> {
+impl<E: MetricData> MetricsBackend<E> {
+    pub async fn load(&self, name: String) -> Option<E> {
         let map = self.stack.lock();
         map.get(name.as_str()).cloned()
     }
 }
 
-
-#[derive(Debug, Clone, clap::Parser)]
-pub struct MetricsSettings
-{
-    #[command(flatten)]
-    graphql: GraphqlServerSettings,
-}
-
-
-/// Configuration for the GraphQl Server
-#[derive(Debug, Clone, clap::Args)]
-pub(crate) struct GraphqlServerSettings {
-    /// Socket where the GraphQL will be listening on for incoming requests.
-    #[arg(short, long = "graphql-addr", env = "METRICS_GRAPHQL_BIND_ADDRESS")]
-    pub address: std::net::SocketAddr,
-    /// Max query depth allowed
-    #[arg(long = "graphql-max-depth", default_value_t = 20, env = "METRICS_GRAPHQL_MAX_DEPTH")]
-    pub max_depth: usize,
-    /// Max query complexity allowed
-    #[arg(
-        long = "graphql-max-complexity",
-        default_value_t = 1000,
-        env = "METRICS_GRAPHQL_MAX_COMPLEXITY"
-    )]
-    pub max_complexity: usize,
-    /// Allowed origins for this server deployment requests.
-    #[arg(long = "graphql-cors-origin")]
-    pub cors_origins: Vec<String>,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub enum MetricsMessage<E> {
+#[derive(Debug)]
+pub enum MetricsMessage<E: MetricData> {
     Load {
-        service_id: Cow<'static, str>,
+        service_id: ServiceId,
+        tx: tokio::sync::oneshot::Sender<Option<E>>,
     },
     Update {
-        service_id: Cow<'static, str>,
-        msg: Data<E>
-    }
+        service_id: ServiceId,
+        data: E,
+    },
 }
 
-impl<E> RelayMessage for MetricsMessage<E> 
-    where E: 'static {}
+impl<E> RelayMessage for MetricsMessage<E> where E: MetricData {}
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Data<E> {
-    duration: Duration,
-    extensions: E,
-}
-
-#[async_graphql::Object]
-impl<E> Data<E> 
-    where E: async_graphql::OutputType + Send + Sync + 'static
-{
-    pub async fn duration(&self) -> u64 {
-        self.duration.as_millis() as u64
-    }
-
-    pub async fn extensions(&self) -> &E {
-        &self.extensions
-    }
-}
-
-
-impl<E> ServiceData for Metrics<E> 
-    where E: core::fmt::Debug + Send + Sync + 'static
-{
+impl<E: MetricData> ServiceData for Metrics<E> {
     const SERVICE_ID: &'static str = "Metrics";
-    type Settings = MetricsSettings;
+    type Settings = ();
     type State = NoState<Self::Settings>;
     type StateOperator = NoOperator<Self::State>;
     type Message = MetricsMessage<E>;
 }
 
-
 #[async_trait::async_trait]
-impl<E> ServiceCore for Metrics<E>
-    where E:  core::fmt::Debug + Send + Sync + 'static,
-{
+impl<E: MetricData> ServiceCore for Metrics<E> {
     fn init(service_state: ServiceStateHandle<Self>) -> Self {
-        let ServiceStateHandle {
-            inbound_relay,
-            overwatch_handle,
-            mut settings_reader,
-            state_updater: _,
-            _lifecycle_handler: _,
-        } = service_state;
-        
         Self {
-            handle: overwatch_handle,
-            inbound_relay,
-            settings: settings_reader.get_updated_settings(),
+            inbound_relay: service_state.inbound_relay,
         }
     }
 
     async fn run(self) {
-        let Self {
-            mut handle,
-            settings,
-            mut inbound_relay,
-        } = self;
+        let Self { mut inbound_relay } = self;
 
         let backend = MetricsBackend::<E>::default();
 
@@ -159,16 +89,18 @@ impl<E> ServiceCore for Metrics<E>
                     msg = inbound_relay.recv() => {
                         if let Some(msg) = msg {
                             match msg {
-                                MetricsMessage::Load { service_id } => {
-                                    // TODO: relay to the caller
-                                    todo!()
+                                MetricsMessage::Load { service_id, tx } => {
+                                    let map = backend.stack.lock();
+                                    if let Err(e) = tx.send(map.get(&service_id).cloned()) {
+                                        tracing::error!("Failed to send metric data for service: {service_id}. data: {:?}", e);
+                                    }
                                 },
-                                MetricsMessage::Update { service_id, msg } => {
+                                MetricsMessage::Update { service_id, data } => {
                                     let mut map = backend.stack.lock();
                                     match map.entry(service_id) {
-                                        std::collections::hash_map::Entry::Occupied(mut val) => val.get_mut().push(msg),
+                                        std::collections::hash_map::Entry::Occupied(mut val) => *val.get_mut() = data,
                                         std::collections::hash_map::Entry::Vacant(val) => {
-                                            val.insert(vec![msg]);
+                                            val.insert(data);
                                         },
                                     }
                                 },
@@ -178,34 +110,5 @@ impl<E> ServiceCore for Metrics<E>
                 }
             }
         });
-
-        // thread for handling graphql query
-        tokio::spawn(async move {
-            
-
-            let mut builder = CorsLayer::new();
-            if settings.graphql.cors_origins.is_empty() {
-                builder = builder.allow_origin(Any);
-            }
-            for origin in settings.graphql.cors_origins {
-                builder = builder.allow_origin(origin.as_str().parse::<HeaderValue>().expect("fail to parse origin"));
-            }
-            let cors = builder
-                .allow_headers([CONTENT_TYPE, USER_AGENT])
-                .allow_methods(Any);
-
-            let router = Router::new()
-                .route("/*path", get(index).post(graphql_handler::<P>))
-                .layer(Extension(schema))
-                .layer(cors)
-                .layer(TraceLayer::new_for_http());
-
-            tracing::info!("Metrics Service GraphQL server listening: {}", settings.graphql.address);
-            Server::bind(&settings.graphql.address)
-                .serve(router.into_make_service())
-                .await
-                .unwrap();
-        });
-
     }
 }
