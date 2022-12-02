7f1e98a4b182e0c96c9571e17d5a2800af72ec5c
Daniel Sanchez Quiros <sanchez.quiros.daniel@gmail.com>
2022-12-02 12:32:25 +0100

    Scratch Network adapter methods

--- nomos-services/consensus/src/network/mod.rs
+++ nomos-services/consensus/src/network/mod.rs
@@ -1,3 +1,6 @@
+use crate::overlay::Overlay;
+use crate::{Approval, Block};
+use futures::Stream;
 use nomos_network::backends::NetworkBackend;
 use nomos_network::NetworkService;
 use overwatch_rs::services::relay::OutboundRelay;
@@ -8,4 +11,9 @@ pub trait NetworkAdapter {
     fn new(
         network_relay: OutboundRelay<<NetworkService<Self::Backend> as ServiceData>::Message>,
     ) -> Self;
+
+    fn proposals_stream(&self) -> Box<dyn Stream<Item = Block>>;
+    fn broadcast_block(&self, block: Block);
+    fn approvals_stream(&self) -> Box<dyn Stream<Item = Approval>>;
+    fn forward_approval(&self, approval: Approval);
 }
