e1a656bd51615bb8ed9ab7ff79d398aa21e8cc40
Daniel Sanchez Quiros <sanchez.quiros.daniel@gmail.com>
2022-12-02 13:01:50 +0100

    Fix waku feature on network crate

---
+++ nomos-services/consensus/src/network/waku.rs
@@ -0,0 +1,37 @@
+use super::NetworkAdapter;
+use crate::{Approval, Block};
+use futures::Stream;
+use nomos_network::backends::Waku;
+use nomos_network::NetworkService;
+use overwatch_rs::services::relay::OutboundRelay;
+use overwatch_rs::services::ServiceData;
+
+pub struct WakuAdapter {
+    waku_backend: Waku,
+}
+
+impl NetworkAdapter for WakuAdapter {
+    type Backend = ();
+
+    fn new(
+        network_relay: OutboundRelay<<NetworkService<Self::Backend> as ServiceData>::Message>,
+    ) -> Self {
+        todo!()
+    }
+
+    fn proposals_stream(&self) -> Box<dyn Stream<Item = Block>> {
+        todo!()
+    }
+
+    fn broadcast_block(&self, block: Block) {
+        todo!()
+    }
+
+    fn approvals_stream(&self) -> Box<dyn Stream<Item = Approval>> {
+        todo!()
+    }
+
+    fn forward_approval(&self, approval: Approval) {
+        todo!()
+    }
+}
