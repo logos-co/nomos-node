5d9fcc2797ceafed0ad2a8ca6eb308a9b7d9f55d
Giacomo Pasini <giacomo@status.im>
2022-12-14 10:46:33 +0100

    address review comments

--- nomos-services/mempool/src/lib.rs
+++ nomos-services/mempool/src/lib.rs
@@ -1,10 +1,13 @@
-//! In this module, and children ones, the 'view lifetime is tied to a logical consensus view,
-//! represented by the `View` struct.
-//! This is done to ensure that all the different data structs used to represent various actors
-//! are always synchronized (i.e. it cannot happen that we accidentally use committees from different views).
-//! It's obviously extremely important that the information contained in `View` is synchronized across different
-//! nodes, but that has to be achieved through different means.
-// mod backends;
+/// std
+use std::fmt::{Debug, Error, Formatter};
+
+/// crates
+use tokio::sync::broadcast::Receiver;
+use tokio::sync::oneshot::Sender;
+
+/// internal
+pub mod backend;
+use backend::Pool;
 use nomos_network::{
     backends::{waku::NetworkEvent, NetworkBackend},
     NetworkService,
@@ -15,10 +18,6 @@ use overwatch_rs::services::{
     state::{NoOperator, NoState},
     ServiceCore, ServiceData, ServiceId,
 };
-use std::fmt::{Debug, Error, Formatter};
-use tokio::sync::broadcast::Receiver;
-use tokio::sync::oneshot::Sender;
-
 pub type BlockId = [u8; 32];
 
 pub struct Mempool<
@@ -28,8 +27,6 @@ pub struct Mempool<
     P: Pool<Tx = Tx, Id = Id> + Send + Sync + 'static,
 > {
     service_state: ServiceStateHandle<Self>,
-    // underlying networking backend. We need this so we can relay and check the types properly
-    // when implementing ServiceCore for CarnotConsensus
     network_relay: Relay<NetworkService<N>>,
     pool: P,
 }
@@ -151,35 +148,4 @@ where
             }
         }
     }
-}
-
-pub trait Pool {
-    type Tx;
-    type Id;
-
-    /// Construct a new empty pool
-    fn new() -> Self;
-
-    /// Add a new transaction to the mempool, for example because we received it from the network
-    fn add_tx(&mut self, tx: Self::Tx, id: Self::Id) -> Result<(), overwatch_rs::DynError>;
-
-    /// Return a view over the transactions contained in the mempool.
-    /// Implementations should provide *at least* all the transactions which have not been marked as
-    /// in a block.
-    /// The hint on the ancestor *can* be used by the implementation to display additional
-    /// transactions that were not included up to that point if available.
-    fn view(&self, ancestor_hint: BlockId) -> Box<dyn Iterator<Item = Self::Tx> + Send>;
-
-    /// Record that a set of transactions were included in a block
-    fn mark_in_block(&mut self, txs: Vec<Self::Id>, block: BlockId);
-
-    /// Signal that a set of transactions can't be possibly requested anymore and can be
-    /// discarded.
-    fn prune(&mut self, txs: Vec<Self::Id>);
-}
-
-#[test]
-fn test() {
-    println!("{:?}", MempoolMsg::AddTx { id: 0, tx: 1 });
-    assert!(false);
 }
