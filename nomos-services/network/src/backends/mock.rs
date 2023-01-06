3ab76c4b0adf6ef62be38bf63924ad89c85a4598
al8n <scygliu1@gmail.com>
2023-01-06 19:09:10 +1300

    WIP

--- nomos-services/network/src/backends/mock.rs
+++ nomos-services/network/src/backends/mock.rs
@@ -1,48 +1,49 @@
+use std::{collections::HashMap, sync::{Arc, Mutex}};
+
 use super::*;
 use overwatch_rs::services::state::NoState;
 use serde::{Deserialize, Serialize};
 use tokio::sync::{
     broadcast::{self, Receiver, Sender},
-    oneshot,
 };
-use tracing::{debug, error};
-use waku_bindings::*;
+use tracing::debug;
 
 const BROADCAST_CHANNEL_BUF: usize = 16;
 
+#[derive(Debug, Clone)]
+pub enum MockMessage {
+    Normal { topic: u64, msg: String },
+    Weighted {
+        weight: usize,
+        msg: String,
+        topic: u64,
+    },
+}
+
 pub struct Mock {
+    #[allow(clippy::type_complexity)]
+    weighted_messages: Arc<Mutex<HashMap<u64, Vec<(usize, String)>>>>,
+    messages: Arc<Mutex<HashMap<u64, Vec<String>>>>,
     message_event: Sender<NetworkEvent>,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct MockConfig {
-    initial_peers: Vec<Multiaddr>,
+    initial_peers: Vec<u64>,
 }
 
 /// Interaction with Mock node
 #[derive(Debug)]
 pub enum MockBackendMessage {
-    /// Send a message to the network
-    Broadcast {
-        message: MockMessage,
-        topic: Option<MockPubSubTopic>,
+    Normal {
+        topic: u64,
+        msg: String,
     },
-    /// Subscribe to a particular Mock topic
-    RelaySubscribe { topic: MockPubSubTopic },
-    /// Unsubscribe from a particular Mock topic
-    RelayUnsubscribe { topic: MockPubSubTopic },
-    /// Retrieve old messages from another peer
-    StoreQuery {
-        query: StoreQuery,
-        peer_id: PeerId,
-        response: oneshot::Sender<StoreResponse>,
-    },
-    /// Send a message using Mock Light Push
-    LightpushPublish {
-        message: MockMessage,
-        topic: Option<MockPubSubTopic>,
-        peer_id: PeerId,
-    },
+    Weighted {
+        topic: u64,
+        weight: usize,
+        msg: String,
+    }
 }
 
 #[derive(Debug)]
@@ -64,100 +65,33 @@ impl NetworkBackend for Mock {
     type NetworkEvent = NetworkEvent;
 
     fn new(config: Self::Config) -> Self {
-        let waku = waku_new(Some(config.inner)).unwrap().start().unwrap();
-        waku.relay_subscribe(None).unwrap();
-        tracing::info!("waku listening on {}", waku.listen_addresses().unwrap()[0]);
-        for peer in &config.initial_peers {
-            if let Err(e) = waku.connect_peer_with_address(peer, None) {
-                tracing::warn!("Could not connect to {peer}: {e}");
-            }
-        }
-
         let message_event = broadcast::channel(BROADCAST_CHANNEL_BUF).0;
-        let tx = message_event.clone();
-        waku_set_event_callback(move |sig| match sig.event() {
-            Event::MockMessage(ref msg_event) => {
-                debug!("received message event");
-                if tx
-                    .send(NetworkEvent::RawMessage(msg_event.waku_message().clone()))
-                    .is_err()
-                {
-                    debug!("no active receiver");
-                }
-            }
-            _ => tracing::warn!("unsupported event"),
-        });
         Self {
-            waku,
+            messages: Arc::new(Mutex::new(config.initial_peers.iter().map(|p| (*p, Vec::new())).collect())),
             message_event,
+            weighted_messages: Arc::new(Mutex::new(config.initial_peers.iter().map(|p| (*p, Vec::new())).collect())),
         }
     }
 
     async fn process(&self, msg: Self::Message) {
         match msg {
-            MockBackendMessage::Broadcast { message, topic } => {
-                match self.waku.relay_publish_message(&message, topic, None) {
-                    Ok(id) => debug!(
-                        "successfully broadcast message with id: {id}, raw contents: {:?}",
-                        message.payload()
-                    ),
-                    Err(e) => tracing::error!(
-                        "could not broadcast message due to {e}, raw contents {:?}",
-                        message.payload()
-                    ),
-                }
+            MockBackendMessage::Normal { topic, msg } => {
+                debug!("processed normal message");
+                let mut normal_msgs = self.messages.lock().unwrap();
+                normal_msgs.entry(topic).or_insert_with(Vec::new).push(msg.clone());
+                drop(normal_msgs);
+                let _ = self.message_event.send(NetworkEvent::RawMessage(MockMessage::Normal {
+                    topic,
+                    msg,
+                }));
             }
-            MockBackendMessage::LightpushPublish {
-                message,
-                topic,
-                peer_id,
-            } => match self.waku.lightpush_publish(&message, topic, peer_id, None) {
-                Ok(id) => debug!(
-                    "successfully published lighpush message with id: {id}, raw contents: {:?}",
-                    message.payload()
-                ),
-                Err(e) => tracing::error!(
-                    "could not publish lightpush message due to {e}, raw contents {:?}",
-                    message.payload()
-                ),
-            },
-            MockBackendMessage::RelaySubscribe { topic } => {
-                match self.waku.relay_subscribe(Some(topic.clone())) {
-                    Ok(_) => debug!("successfully subscribed to topic {:?}", topic),
-                    Err(e) => {
-                        tracing::error!("could not subscribe to topic {:?} due to {e}", topic)
-                    }
-                }
+            MockBackendMessage::Weighted { topic, weight, msg} => {
+                debug!("processed weighted message");
+                let mut weighted_msgs = self.weighted_messages.lock().unwrap();
+                weighted_msgs.entry(topic).or_insert_with(Vec::new).push((weight, msg.clone()));
+                drop(weighted_msgs);
+                let _ = self.message_event.send(NetworkEvent::RawMessage(MockMessage::Weighted { weight, msg, topic }));
             }
-            MockBackendMessage::RelayUnsubscribe { topic } => {
-                match self.waku.relay_unsubscribe(Some(topic.clone())) {
-                    Ok(_) => debug!("successfully unsubscribed to topic {:?}", topic),
-                    Err(e) => {
-                        tracing::error!("could not unsubscribe to topic {:?} due to {e}", topic)
-                    }
-                }
-            }
-            MockBackendMessage::StoreQuery {
-                query,
-                peer_id,
-                response,
-            } => match self.waku.store_query(&query, &peer_id, None) {
-                Ok(res) => {
-                    debug!(
-                        "successfully retrieved stored messages with options {:?}",
-                        query
-                    );
-                    response
-                        .send(res)
-                        .unwrap_or_else(|_| error!("client hung up store query handle"));
-                }
-                Err(e) => {
-                    error!(
-                        "could not retrieve store messages due to {e}, options: {:?}",
-                        query
-                    )
-                }
-            },
         };
     }
 
