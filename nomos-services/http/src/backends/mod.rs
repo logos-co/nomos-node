6a7fc998fe102987a6a455c821ecf159f4e2328a
Gusto Bacvinka <augustinas.bacvinka@gmail.com>
2023-01-08 23:16:41 +0200

    HttpMsg definition

--- nomos-services/http/src/backends/mod.rs
+++ nomos-services/http/src/backends/mod.rs
@@ -1,15 +1,18 @@
+#[cfg(feature = "http")]
+pub mod axum;
+
 use std::fmt::Debug;
 
 use overwatch_rs::services::state::ServiceState;
-
-#[cfg(feature = "http")]
-pub mod axum;
 
 #[async_trait::async_trait]
 pub trait HttpServer: Send + Sync + 'static {
     type Config: Clone + Debug + Send + Sync + 'static;
     type State: ServiceState<Settings = Self::Config> + Clone;
-    type Message: Debug + Send + Sync + 'static;
+    type Request: Send;
+    type Response: Send;
 
     fn new(config: Self::Config) -> Self;
+    fn add_route(&self);
+    async fn run(&self) -> Result<(), overwatch_rs::DynError>;
 }
--- nomos-services/http/src/lib.rs
+++ nomos-services/http/src/lib.rs
@@ -1,15 +1,17 @@
 mod backends;
 
 // std
+use std::fmt::{self, Debug};
 
 // crates
 use overwatch_rs::services::{
     handle::ServiceStateHandle,
-    relay::NoMessage,
-    state::{NoOperator, ServiceState},
+    relay::{InboundRelay, NoMessage, RelayMessage},
+    state::{NoOperator, NoState, ServiceState},
     ServiceCore, ServiceData, ServiceId,
 };
 use serde::{Deserialize, Serialize};
+use tokio::sync::{broadcast::Sender, oneshot::Receiver};
 
 // internal
 use backends::HttpServer;
@@ -19,35 +21,84 @@ pub struct Config<B: HttpServer> {
     pub backend: B::Config,
 }
 
-pub struct State<B: HttpServer> {
-    _backend: B::State,
-}
-
 pub struct HttpService<B: HttpServer> {
     backend: B,
-    service_state: ServiceStateHandle<Self>,
+    inbound_relay: InboundRelay<HttpMsg<B>>,
 }
 
 impl<B: HttpServer> ServiceData for HttpService<B> {
     const SERVICE_ID: ServiceId = "Http";
     type Settings = Config<B>;
-    type State = State<B>;
+    type State = NoState<Self::Settings>;
     type StateOperator = NoOperator<Self::State>;
-    type Message = NoMessage;
+    type Message = HttpMsg<B>;
+}
+
+pub enum HttpMsg<B: HttpServer> {
+    AddHandler {
+        service_id: ServiceId,
+        route: String, // /metrics/ {GET/POST}
+        rx: Sender<Receiver<B::Response>>,
+    },
+}
+
+impl<B: HttpServer + 'static> RelayMessage for HttpMsg<B> {}
+
+impl<B: HttpServer> Debug for HttpMsg<B> {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::AddHandler {
+                service_id,
+                route,
+                rx,
+            } => write!(
+                fmt,
+                "HttpMsg::AddHandler{{ sender: {:?}, route: {:?}, rx: {:?} }}",
+                service_id, route, rx
+            ),
+        }
+    }
 }
 
 #[async_trait::async_trait]
 impl<B: HttpServer> ServiceCore for HttpService<B> {
     fn init(service_state: ServiceStateHandle<Self>) -> Result<Self, overwatch_rs::DynError> {
+        let inbound_relay = service_state.inbound_relay;
         Ok(Self {
             backend: <B as HttpServer>::new(
                 service_state.settings_reader.get_updated_settings().backend,
             ),
-            service_state,
+            inbound_relay,
         })
     }
 
     async fn run(mut self) -> Result<(), overwatch_rs::DynError> {
+        let Self {
+            mut backend,
+            mut inbound_relay,
+        } = self;
+
+        // 1.
+        // Create router.
+        // Pass it to the backend.
+        //
+        // 2.
+        // Get a router object.
+        // Store it in this scope.
+
+        // backend.add_route(todo!());
+
+        loop {
+            // select
+            // handle the HttpMsg
+            while let Some(HttpMsg::AddHandler {
+                service_id,
+                route,
+                rx,
+            }) = inbound_relay.recv().await
+            {}
+            // backend.run()
+        }
         Ok(())
     }
 }
@@ -57,22 +108,5 @@ impl<B: HttpServer> Clone for Config<B> {
         Self {
             backend: self.backend.clone(),
         }
-    }
-}
-
-impl<B: HttpServer> Clone for State<B> {
-    fn clone(&self) -> Self {
-        Self {
-            _backend: self._backend.clone(),
-        }
-    }
-}
-
-impl<B: HttpServer> ServiceState for State<B> {
-    type Settings = Config<B>;
-    type Error = <B::State as ServiceState>::Error;
-
-    fn from_settings(settings: &Self::Settings) -> Result<Self, Self::Error> {
-        B::State::from_settings(&settings.backend).map(|_backend| Self { _backend })
     }
 }
