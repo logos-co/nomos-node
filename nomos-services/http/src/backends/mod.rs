acea68fb7d102fc15f29c6af0109c53a58c71839
Gusto Bacvinka <augustinas.bacvinka@gmail.com>
2023-01-10 01:00:11 +0200

    Shared axum router and router modification methods

--- nomos-services/http/src/backends/mod.rs
+++ nomos-services/http/src/backends/mod.rs
@@ -7,22 +7,26 @@ use overwatch_rs::{
     services::{state::ServiceState, ServiceId},
     DynError,
 };
+use tokio::sync::broadcast::Sender;
 
-use crate::Route;
+use crate::{HttpRequest, Route};
 
 #[async_trait::async_trait]
 pub trait HttpBackend: Send + Sync + 'static {
     type Config: Clone + Debug + Send + Sync + 'static;
     type State: ServiceState<Settings = Self::Config> + Clone;
-    type Request: Send;
-    type Response: Send;
-    type Handler: Send + Sync + Sized;
+    type Request: Clone + Send;
+    type Response: Clone + Send;
 
     fn new(config: Self::Config) -> Result<Self, DynError>
     where
         Self: Sized;
-    fn add_route(&self, service_id: ServiceId, route: Route<Self>)
-    where
+    fn add_route(
+        &self,
+        service_id: ServiceId,
+        route: Route,
+        req_stream: Sender<HttpRequest<Self::Request, Self::Response>>,
+    ) where
         Self: Sized;
     async fn run(&self) -> Result<(), overwatch_rs::DynError>;
 }
