707f2d56e59a65772feb1843aa4bc44772998456
Daniel Sanchez Quiros <sanchez.quiros.daniel@gmail.com>
2023-01-09 16:21:30 +0100

    Remove transaction placeholder

--- nomos-core/src/transactions/mod.rs
+++ nomos-core/src/transactions/mod.rs
@@ -1,16 +1,3 @@
 // std
-use std::error::Error;
 // crates
-use serde::{Deserialize, Serialize};
 // internal
-
-#[derive(Serialize, Deserialize)]
-pub enum Transaction {
-    None,
-}
-
-impl Transaction {
-    pub fn from_bytes(_bytes: &[u8]) -> Result<Self, Box<dyn Error>> {
-        Ok(Self::None)
-    }
-}
