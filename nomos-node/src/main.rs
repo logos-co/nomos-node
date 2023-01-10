5deb6a3fe42651f9d4bd7c6f189e6933a2be1171
Giacomo Pasini <giacomo@status.im>
2023-01-10 15:08:55 +0100

    add metrics service

--- nomos-node/src/main.rs
+++ nomos-node/src/main.rs
@@ -1,13 +1,16 @@
 use clap::Parser;
-use color_eyre::eyre::Result;
+use color_eyre::eyre::{eyre, Result};
 use nomos_log::Logger;
-use nomos_network::{backends::Waku, NetworkService};
+use nomos_network::{backends::waku::Waku, NetworkService};
 use overwatch_derive::*;
 use overwatch_rs::{
     overwatch::*,
     services::{handle::ServiceHandle, ServiceData},
 };
 use serde::Deserialize;
+
+#[cfg(feature = "metrics")]
+use metrics::{backend::map::MapMetricsBackend, types::MetricsData, MetricsService};
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -21,12 +24,16 @@ struct Args {
 struct Config {
     log: <Logger as ServiceData>::Settings,
     network: <NetworkService<Waku> as ServiceData>::Settings,
+    #[cfg(feature = "metrics")]
+    metrics: <MetricsService<MapMetricsBackend<MetricsData>> as ServiceData>::Settings,
 }
 
 #[derive(Services)]
 struct Services {
     logging: ServiceHandle<Logger>,
     network: ServiceHandle<NetworkService<Waku>>,
+    #[cfg(feature = "metrics")]
+    metrics: ServiceHandle<MetricsService<MapMetricsBackend<MetricsData>>>,
 }
 
 fn main() -> Result<()> {
@@ -37,9 +44,12 @@ fn main() -> Result<()> {
         ServicesServiceSettings {
             network: config.network,
             logging: config.log,
+            #[cfg(feature = "metrics")]
+            metrics: config.metrics,
         },
         None,
-    );
+    )
+    .map_err(|e| eyre!("Error encountered: {}", e))?;
     app.wait_finished();
     Ok(())
 }
