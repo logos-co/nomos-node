300280735ef8d75edd072d0456b21e14e709efa5
danielsanchezq <sanchez.quiros.daniel@gmail.com>
2023-01-17 12:00:09 +0100

    Use linked hashmap

--- nomos-services/mempool/src/backend/mockpool.rs
+++ nomos-services/mempool/src/backend/mockpool.rs
@@ -1,4 +1,5 @@
 // std
+use linked_hash_map::LinkedHashMap;
 use std::collections::BTreeMap;
 // crates
 // internal
@@ -7,19 +8,15 @@ use nomos_core::block::{BlockHeader, BlockId};
 
 /// A mock mempool implementation that stores all transactions in memory in the order received.
 pub struct MockPool<Id, Tx> {
-    pending_txs_lookup: BTreeMap<Id, usize>,
-    pending_txs_by_index: Vec<Tx>,
-    in_block_txs: BTreeMap<BlockId, Vec<Tx>>,
-    in_block_txs_lookup: BTreeMap<Id, BlockId>,
+    pending_txs: LinkedHashMap<Id, Tx>,
+    in_block_txs: LinkedHashMap<BlockId, Vec<Tx>>,
 }
 
 impl<Id, Tx> Default for MockPool<Id, Tx> {
     fn default() -> Self {
         Self {
-            pending_txs_lookup: BTreeMap::new(),
-            pending_txs_by_index: Vec::new(),
-            in_block_txs: BTreeMap::new(),
-            in_block_txs_lookup: BTreeMap::new(),
+            pending_txs: LinkedHashMap::new(),
+            in_block_txs: LinkedHashMap::new(),
         }
     }
 }
@@ -45,37 +42,33 @@ where
 
     fn add_tx(&mut self, tx: Self::Tx) -> Result<(), overwatch_rs::DynError> {
         let id = Id::from(tx.clone());
-        if self.pending_txs_lookup.contains_key(&id) || self.in_block_txs_lookup.contains_key(&id) {
+        if self.pending_txs.contains_key(&id) || self.in_block_txs.contains_key(&id) {
             return Ok(());
         }
-        let index = self.pending_txs_by_index.len();
-        self.pending_txs_lookup.insert(id, index);
-        self.pending_txs_by_index.push(tx);
+        self.pending_txs.insert(id, tx);
         Ok(())
     }
 
     fn view(&self, _ancestor_hint: BlockId) -> Box<dyn Iterator<Item = Self::Tx> + Send> {
         // we need to have an owned version of the iterator to bypass adding a lifetime bound to the return iterator type
         #[allow(clippy::unnecessary_to_owned)]
-        Box::new(self.pending_txs_by_index.to_vec().into_iter())
+        Box::new(self.pending_txs.to_vec().into_iter())
     }
 
     fn mark_in_block(&mut self, txs: &[Self::Id], block: BlockHeader) {
-        let mut txs_in_block = Vec::new();
+        let mut txs_in_block = Vec::with_capacity(txs.len());
         for tx_id in txs.iter() {
-            if let Some(index) = self.pending_txs_lookup.remove(tx_id) {
-                let tx = self.pending_txs_by_index.remove(index);
+            if let Some(tx) = self.pending_txs.remove(tx_id) {
                 txs_in_block.push(tx);
             }
         }
-        self.in_block_txs.insert(block.id(), txs_in_block);
+        let block_entry = self.in_block_txs.entry(block.id).or_default();
+        block_entry.append(&mut txs_in_block);
     }
 
     fn prune(&mut self, txs: &[Self::Id]) {
         for tx_id in txs {
-            if let Some(i) = self.pending_txs_lookup.remove(tx_id) {
-                self.pending_txs_by_index.remove(i);
-            }
+            self.pending_txs.remove(tx_id)
         }
     }
 }
