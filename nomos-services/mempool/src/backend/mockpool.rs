d8d447269fbfed3acb653a52d20e742b8c9bb621
danielsanchezq <sanchez.quiros.daniel@gmail.com>
2023-01-17 12:44:20 +0100

    Fix bounds

--- nomos-services/mempool/src/backend/mockpool.rs
+++ nomos-services/mempool/src/backend/mockpool.rs
@@ -1,6 +1,7 @@
 // std
 use linked_hash_map::LinkedHashMap;
 use std::collections::BTreeMap;
+use std::hash::Hash;
 // crates
 // internal
 use crate::backend::MemPool;
@@ -9,19 +10,27 @@ use nomos_core::block::{BlockHeader, BlockId};
 /// A mock mempool implementation that stores all transactions in memory in the order received.
 pub struct MockPool<Id, Tx> {
     pending_txs: LinkedHashMap<Id, Tx>,
-    in_block_txs: LinkedHashMap<BlockId, Vec<Tx>>,
+    in_block_txs: BTreeMap<BlockId, Vec<Tx>>,
+    in_block_txs_by_id: BTreeMap<Id, BlockId>,
 }
 
-impl<Id, Tx> Default for MockPool<Id, Tx> {
+impl<Id, Tx> Default for MockPool<Id, Tx>
+where
+    Id: Eq + Hash,
+{
     fn default() -> Self {
         Self {
             pending_txs: LinkedHashMap::new(),
-            in_block_txs: LinkedHashMap::new(),
+            in_block_txs: BTreeMap::new(),
+            in_block_txs_by_id: BTreeMap::new(),
         }
     }
 }
 
-impl<Id, Tx> MockPool<Id, Tx> {
+impl<Id, Tx> MockPool<Id, Tx>
+where
+    Id: Eq + Hash,
+{
     pub fn new() -> Self {
         Default::default()
     }
@@ -29,8 +38,8 @@ impl<Id, Tx> MockPool<Id, Tx> {
 
 impl<Id, Tx> MemPool for MockPool<Id, Tx>
 where
-    Id: From<Tx> + PartialOrd + Ord,
-    Tx: Clone + Send + Sync + 'static,
+    Id: From<Tx> + PartialOrd + Ord + Eq + Hash + Send + Clone,
+    Tx: Clone + Send + Sync + 'static + Hash,
 {
     type Settings = ();
     type Tx = Tx;
@@ -42,7 +51,7 @@ where
 
     fn add_tx(&mut self, tx: Self::Tx) -> Result<(), overwatch_rs::DynError> {
         let id = Id::from(tx.clone());
-        if self.pending_txs.contains_key(&id) || self.in_block_txs.contains_key(&id) {
+        if self.pending_txs.contains_key(&id) || self.in_block_txs_by_id.contains_key(&id) {
             return Ok(());
         }
         self.pending_txs.insert(id, tx);
@@ -51,8 +60,9 @@ where
 
     fn view(&self, _ancestor_hint: BlockId) -> Box<dyn Iterator<Item = Self::Tx> + Send> {
         // we need to have an owned version of the iterator to bypass adding a lifetime bound to the return iterator type
-        #[allow(clippy::unnecessary_to_owned)]
-        Box::new(self.pending_txs.to_vec().into_iter())
+        #[allow(clippy::needless_collect)]
+        let pending_txs: Vec<Tx> = self.pending_txs.values().cloned().collect();
+        Box::new(pending_txs.into_iter())
     }
 
     fn mark_in_block(&mut self, txs: &[Self::Id], block: BlockHeader) {
@@ -62,13 +72,15 @@ where
                 txs_in_block.push(tx);
             }
         }
-        let block_entry = self.in_block_txs.entry(block.id).or_default();
+        let block_entry = self.in_block_txs.entry(block.id()).or_default();
+        self.in_block_txs_by_id
+            .extend(txs.iter().cloned().map(|tx| (tx, block.id())));
         block_entry.append(&mut txs_in_block);
     }
 
     fn prune(&mut self, txs: &[Self::Id]) {
         for tx_id in txs {
-            self.pending_txs.remove(tx_id)
+            self.pending_txs.remove(tx_id);
         }
     }
 }
