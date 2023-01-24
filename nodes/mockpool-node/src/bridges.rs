f78354bb1f7cd46ad921dbae03ba79effa0730df
danielsanchezq <sanchez.quiros.daniel@gmail.com>
2023-01-24 13:46:38 +0100

    Add waku network info bridge

--- nodes/mockpool-node/src/bridges.rs
+++ nodes/mockpool-node/src/bridges.rs
@@ -2,7 +2,6 @@
 
 // crates
 use tokio::sync::oneshot;
-
 // internal
 use crate::tx::{Tx, TxId};
 use nomos_http::backends::axum::AxumBackend;
@@ -11,6 +10,8 @@ use nomos_http::http::{HttpMethod, HttpRequest};
 use nomos_mempool::backend::mockpool::MockPool;
 use nomos_mempool::network::adapters::waku::WakuAdapter;
 use nomos_mempool::{MempoolMetrics, MempoolMsg, MempoolService};
+use nomos_network::backends::waku::{Waku, WakuBackendMessage, WakuInfo};
+use nomos_network::{NetworkMsg, NetworkService};
 
 pub fn mempool_metrics_bridge(
     handle: overwatch_rs::overwatch::handle::OverwatchHandle,
@@ -38,7 +39,43 @@ pub fn mempool_metrics_bridge(
                 .unwrap();
             let metrics: MempoolMetrics = receiver.await.unwrap();
             res_tx
+                // TODO: use serde to serialize metrics
                 .send(format!("{{\"pending_tx\": {}}}", metrics.pending_txs).into())
+                .await
+                .unwrap();
+        }
+        Ok(())
+    }))
+}
+
+pub fn waku_info_bridge(
+    handle: overwatch_rs::overwatch::handle::OverwatchHandle,
+) -> HttpBridgeRunner {
+    Box::new(Box::pin(async move {
+        let (waku_channel, mut http_request_channel) =
+            build_http_bridge::<NetworkService<Waku>, AxumBackend, _>(
+                handle,
+                HttpMethod::GET,
+                "network/info",
+            )
+            .await
+            .unwrap();
+
+        while let Some(HttpRequest { res_tx, .. }) = http_request_channel.recv().await {
+            let (sender, receiver) = oneshot::channel();
+            waku_channel
+                .send(NetworkMsg::Process(WakuBackendMessage::Info {
+                    reply_channel: sender,
+                }))
+                .await
+                .unwrap();
+            let waku_info: WakuInfo = receiver.await.unwrap();
+            res_tx
+                .send(
+                    serde_json::to_vec(&waku_info)
+                        .expect("Serializing of waku info message should not fail")
+                        .into(),
+                )
                 .await
                 .unwrap();
         }
