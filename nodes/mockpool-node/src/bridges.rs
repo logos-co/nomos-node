5f21a2734a3c8ecf880a52a1259f01f039c7939c
gusto <bacv@users.noreply.github.com>
2023-02-07 17:50:38 +0200

    Add endpoint to make a new peer conn for mockpool node (#68)
    * Add enpoint to make a new peer conn for mockpool node
    
    * Pass multiple addresses in connection request
    
    * Join async reqs to network service, cleanup
    
    * Collect and join reqs

--- nodes/mockpool-node/src/bridges.rs
+++ nodes/mockpool-node/src/bridges.rs
@@ -1,9 +1,8 @@
 // std
+
 // crates
-use tokio::sync::oneshot;
-use tracing::debug;
-// internal
-use crate::tx::{Tx, TxId};
+use futures::future::join_all;
+use multiaddr::Multiaddr;
 use nomos_http::backends::axum::AxumBackend;
 use nomos_http::bridge::{build_http_bridge, HttpBridgeRunner};
 use nomos_http::http::{HttpMethod, HttpRequest};
@@ -12,6 +11,11 @@ use nomos_mempool::network::adapters::waku::WakuAdapter;
 use nomos_mempool::{MempoolMetrics, MempoolMsg, MempoolService};
 use nomos_network::backends::waku::{Waku, WakuBackendMessage, WakuInfo};
 use nomos_network::{NetworkMsg, NetworkService};
+use tokio::sync::oneshot;
+use tracing::debug;
+
+// internal
+use crate::tx::{Tx, TxId};
 
 pub fn mempool_metrics_bridge(
     handle: overwatch_rs::overwatch::handle::OverwatchHandle,
@@ -115,6 +119,44 @@ pub fn waku_info_bridge(
                 )
                 .await
                 .unwrap();
+        }
+        Ok(())
+    }))
+}
+
+pub fn waku_add_conn_bridge(
+    handle: overwatch_rs::overwatch::handle::OverwatchHandle,
+) -> HttpBridgeRunner {
+    Box::new(Box::pin(async move {
+        let (waku_channel, mut http_request_channel) = build_http_bridge::<
+            NetworkService<Waku>,
+            AxumBackend,
+            _,
+        >(handle, HttpMethod::POST, "conn")
+        .await
+        .unwrap();
+
+        while let Some(HttpRequest {
+            res_tx, payload, ..
+        }) = http_request_channel.recv().await
+        {
+            if let Some(payload) = payload {
+                if let Ok(addrs) = serde_json::from_slice::<Vec<Multiaddr>>(&payload) {
+                    let reqs: Vec<_> = addrs
+                        .into_iter()
+                        .map(|addr| {
+                            waku_channel.send(NetworkMsg::Process(
+                                WakuBackendMessage::ConnectPeer { addr },
+                            ))
+                        })
+                        .collect();
+
+                    join_all(reqs).await;
+                }
+                res_tx.send(b"".to_vec().into()).await.unwrap();
+            } else {
+                debug!("Invalid payload, {:?}. Should not be empty", payload);
+            }
         }
         Ok(())
     }))
