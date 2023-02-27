ca0fb172b952556ec7ec1b9a516603810185f233
Daniel Sanchez Quiros <sanchez.quiros.daniel@gmail.com>
2023-02-27 13:36:48 +0100

    Use node steps

--- simulations/src/node/mod.rs
+++ simulations/src/node/mod.rs
@@ -1,8 +1,15 @@
+// std
+use std::time::Duration;
+// crates
+// internal
+
 pub type NodeId = usize;
+pub type StepTime = Duration;
 
 pub trait Node {
     type Settings;
+    type Step;
     fn new(settings: Self::Settings) -> Self;
     fn id(&self) -> NodeId;
-    fn run(&mut self);
+    fn run_step(&mut self, steps: Self::Step) -> StepTime;
 }
