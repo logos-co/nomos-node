088ce63442a26be956e1aded60d45880fac17f9c
danielsanchezq <sanchez.quiros.daniel@gmail.com>
2023-03-13 14:27:11 +0100

    Add checks on valid votes

--- nomos-core/src/vote/carnot.rs
+++ nomos-core/src/vote/carnot.rs
@@ -2,11 +2,15 @@
 // TODO: Well, remove this when we actually use the fields from the specification
 // std
 
+use std::collections::HashSet;
 // crates
 use futures::{Stream, StreamExt};
 // internal
 use crate::block::BlockId;
+use crate::crypto::PublicKey;
 use crate::vote::Tally;
+
+pub type NodeId = PublicKey;
 
 pub enum QuorumCertificate {
     Simple(SimpleQuorumCertificate),
@@ -35,7 +39,7 @@ pub struct AggregatedQuorumCertificate {
 pub struct Vote {
     block: BlockId,
     view: u64,
-    voter: usize, // TODO: this should be some id, probably the node pk
+    voter: NodeId, // TODO: this should be some id, probably the node pk
     qc: Option<QuorumCertificate>,
 }
 
@@ -47,8 +51,8 @@ impl Vote {
 
 #[derive(thiserror::Error, Debug)]
 pub enum CarnotTallyError {
-    #[error("Received invalid vote")]
-    InvalidVote,
+    #[error("Received invalid vote: {0}")]
+    InvalidVote(String),
     #[error("Did not receive enough votes")]
     InsufficientVotes,
 }
@@ -56,6 +60,8 @@ pub enum CarnotTallyError {
 #[derive(Clone)]
 pub struct CarnotTallySettings {
     threshold: usize,
+    // TODO: this probably should be dynamic and should change with the view (?)
+    participating_nodes: HashSet<NodeId>,
 }
 
 pub struct CarnotTally {
@@ -79,10 +85,25 @@ impl Tally for CarnotTally {
         mut vote_stream: S,
     ) -> Result<Self::Outcome, Self::TallyError> {
         let mut approved = 0usize;
+        let mut seen = HashSet::new();
         while let Some(vote) = vote_stream.next().await {
+            // check vote view is valid
             if !vote.valid_view(view) {
-                return Err(CarnotTallyError::InvalidVote);
+                return Err(CarnotTallyError::InvalidVote("Invalid view".to_string()));
             }
+            // check for duplicated votes
+            if seen.contains(&vote.voter) {
+                return Err(CarnotTallyError::InvalidVote(
+                    "Double voted node".to_string(),
+                ));
+            }
+            // check for individual nodes votes
+            if !self.settings.participating_nodes.contains(&vote.voter) {
+                return Err(CarnotTallyError::InvalidVote(
+                    "Non-participating node".to_string(),
+                ));
+            }
+            seen.insert(vote.voter);
             approved += 1;
             if approved >= self.settings.threshold {
                 return Ok(QuorumCertificate::Simple(SimpleQuorumCertificate {
