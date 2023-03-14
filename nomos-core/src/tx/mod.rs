a0049f304851734bb6e1f35ff372115149a4bbb5
danielsanchezq <sanchez.quiros.daniel@gmail.com>
2023-03-14 18:34:33 +0100

    Impl Transaction trait

--- nomos-core/src/tx/mod.rs
+++ nomos-core/src/tx/mod.rs
@@ -1,4 +1,20 @@
+// std
+// crates
+use bytes::Bytes;
+// internal
+
 pub mod carnot;
 #[cfg(feature = "mock")]
 pub mod mock;
 mod transaction;
+
+pub type TransactionHasher<T> = fn(&T) -> <T as Transaction>::Hash;
+
+pub trait Transaction {
+    const HASHER: TransactionHasher<Self>;
+    type Hash;
+    fn hash(&self) -> Self::Hash {
+        Self::HASHER(self)
+    }
+    fn as_bytes(&self) -> Bytes;
+}
