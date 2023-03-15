eaeb5308328f9f9a4962048e0c1cd917614ab863
danielsanchezq <sanchez.quiros.daniel@gmail.com>
2023-03-15 16:23:46 +0100

    Fix tests

--- nodes/mockpool-node/src/tx.rs
+++ nodes/mockpool-node/src/tx.rs
@@ -18,21 +18,3 @@ impl Transaction for Tx {
         self.0.as_bytes().to_vec().into()
     }
 }
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn test_txid() {
-        let tx = Tx("test".to_string());
-        let txid = TxId::from(&tx);
-        assert_eq!(
-            txid.0,
-            [
-                39, 227, 252, 176, 211, 134, 68, 39, 134, 158, 47, 7, 82, 40, 169, 232, 168, 118,
-                240, 103, 84, 146, 127, 64, 60, 196, 126, 142, 172, 156, 124, 78
-            ]
-        );
-    }
-}
