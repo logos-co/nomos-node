d7b3b32b767b7870b2ac2d0934a092a233a25501
danielsanchezq <sanchez.quiros.daniel@gmail.com>
2023-03-16 17:02:46 +0100

    Refactor redundant Carnot in CarnotTx

--- nomos-core/src/tx/carnot/mod.rs
+++ nomos-core/src/tx/carnot/mod.rs
@@ -9,23 +9,23 @@ use crate::tx::{Transaction, TransactionHasher};
 mod transaction;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
-pub enum CarnotTx {
+pub enum Tx {
     Transfer(TransferTransaction),
 }
 
 // TODO: We should probably abstract the de/serialization of the transaction as it s done in transaction.rs
-fn hash_carnot_tx(tx: &CarnotTx) -> [u8; 32] {
+fn hash_carnot_tx(tx: &Tx) -> [u8; 32] {
     use blake2::{
         digest::{consts::U32, Digest},
         Blake2b,
     };
     let mut hasher = Blake2b::<U32>::new();
-    hasher.update(<CarnotTx as Transaction>::as_bytes(tx));
+    hasher.update(<Tx as Transaction>::as_bytes(tx));
     let res = hasher.finalize();
     res.into()
 }
 
-impl Transaction for CarnotTx {
+impl Transaction for Tx {
     const HASHER: TransactionHasher<Self> = hash_carnot_tx;
     type Hash = [u8; 32];
 
