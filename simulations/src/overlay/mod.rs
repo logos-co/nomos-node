00fa2643ae09293e415314469342a6b43df2efcd
Gusto Bacvinka <augustinas.bacvinka@gmail.com>
2023-03-20 15:29:02 +0200

    Cleanupt

--- simulations/src/overlay/mod.rs
+++ simulations/src/overlay/mod.rs
@@ -5,7 +5,6 @@ use std::collections::{BTreeSet, HashMap};
 // crates
 use rand::Rng;
 // internal
-use crate::node::carnot::CarnotStep;
 use crate::node::{CommitteeId, NodeId};
 
 pub type Committee = BTreeSet<NodeId>;
