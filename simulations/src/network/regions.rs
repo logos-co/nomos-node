b99f9077b8a86b64d7c0954391a41e2dcc3dfa88
Gusto Bacvinka <augustinas.bacvinka@gmail.com>
2023-05-08 12:45:02 +0300

    Testcase for region distribution

--- simulations/src/network/regions.rs
+++ simulations/src/network/regions.rs
@@ -70,13 +70,107 @@ pub fn create_regions<R: Rng>(
     let mut region_nodes = node_ids.to_vec();
     region_nodes.shuffle(rng);
 
-    network_settings
+    let regions = network_settings
         .regions
+        .clone()
+        .into_iter()
+        .collect::<Vec<_>>();
+
+    let last_region_index = regions.len() - 1;
+
+    regions
         .iter()
-        .map(|(region, distribution)| {
-            let node_count = (node_ids.len() as f32 * distribution).round() as usize;
-            let nodes = region_nodes.drain(..node_count).collect::<Vec<_>>();
-            (*region, nodes)
+        .enumerate()
+        .map(|(i, (region, distribution))| {
+            if i < last_region_index {
+                let node_count = (node_ids.len() as f32 * distribution).round() as usize;
+                let nodes = region_nodes.drain(..node_count).collect::<Vec<_>>();
+                (*region, nodes)
+            } else {
+                // Assign the remaining nodes to the last region.
+                (*region, region_nodes.clone())
+            }
         })
         .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use rand::rngs::mock::StepRng;
+
+    use crate::{
+        network::{
+            regions::{create_regions, Region},
+            NetworkSettings,
+        },
+        node::NodeId,
+    };
+
+    #[test]
+    fn create_regions_precision() {
+        struct TestCase {
+            node_count: usize,
+            distributions: Vec<f32>,
+        }
+
+        let test_cases = vec![
+            TestCase {
+                node_count: 10,
+                distributions: vec![0.5, 0.3, 0.2],
+            },
+            TestCase {
+                node_count: 7,
+                distributions: vec![0.6, 0.4],
+            },
+            TestCase {
+                node_count: 20,
+                distributions: vec![0.4, 0.3, 0.2, 0.1],
+            },
+            TestCase {
+                node_count: 23,
+                distributions: vec![0.4, 0.3, 0.3],
+            },
+            TestCase {
+                node_count: 111,
+                distributions: vec![0.3, 0.3, 0.3, 0.1],
+            },
+            TestCase {
+                node_count: 73,
+                distributions: vec![0.3, 0.2, 0.2, 0.2, 0.1],
+            },
+        ];
+        let mut rng = StepRng::new(1, 0);
+
+        for tcase in test_cases.iter() {
+            let nodes = (0..tcase.node_count)
+                .map(Into::into)
+                .collect::<Vec<NodeId>>();
+
+            let available_regions = vec![
+                Region::NorthAmerica,
+                Region::Europe,
+                Region::Asia,
+                Region::Africa,
+                Region::SouthAmerica,
+                Region::Australia,
+            ];
+
+            let mut region_distribution = HashMap::new();
+            for (region, &dist) in available_regions.iter().zip(&tcase.distributions) {
+                region_distribution.insert(*region, dist);
+            }
+
+            let settings = NetworkSettings {
+                network_behaviors: HashMap::new(),
+                regions: region_distribution,
+            };
+
+            let regions = create_regions(&nodes, &mut rng, &settings);
+
+            let total_nodes_in_regions = regions.values().map(|v| v.len()).sum::<usize>();
+            assert_eq!(total_nodes_in_regions, nodes.len());
+        }
+    }
 }
