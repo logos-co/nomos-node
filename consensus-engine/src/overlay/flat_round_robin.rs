ae81dde9abaab6d5a3fdab7af477efb021731f78
Giacomo Pasini <g.pasini98@gmail.com>
2023-05-23 10:36:28 +0200

    move overlay to consensus engine

---
+++ consensus-engine/src/overlay/flat_round_robin.rs
@@ -0,0 +1,65 @@
+use crate::{Committee, NodeId, Overlay, View};
+
+#[derive(Clone, Debug)]
+/// Flat overlay with a single committee and round robin leader selection.
+pub struct FlatRoundRobin {
+    nodes: Vec<NodeId>,
+}
+
+impl Overlay for FlatRoundRobin {
+    fn new(nodes: Vec<NodeId>) -> Self {
+        Self { nodes }
+    }
+
+    fn root_committee(&self) -> crate::Committee {
+        self.nodes.clone().into_iter().collect()
+    }
+
+    fn rebuild(&mut self, _timeout_qc: crate::TimeoutQc) {
+        todo!()
+    }
+
+    fn is_member_of_child_committee(&self, _parent: NodeId, _child: NodeId) -> bool {
+        false
+    }
+
+    fn is_member_of_root_committee(&self, _id: NodeId) -> bool {
+        true
+    }
+
+    fn is_member_of_leaf_committee(&self, _id: NodeId) -> bool {
+        true
+    }
+
+    fn is_child_of_root_committee(&self, _id: NodeId) -> bool {
+        false
+    }
+
+    fn parent_committee(&self, _id: NodeId) -> crate::Committee {
+        Committee::new()
+    }
+
+    fn node_committee(&self, _id: NodeId) -> crate::Committee {
+        self.nodes.clone().into_iter().collect()
+    }
+
+    fn child_committees(&self, _id: NodeId) -> Vec<crate::Committee> {
+        vec![]
+    }
+
+    fn leaf_committees(&self, _id: NodeId) -> Vec<crate::Committee> {
+        vec![self.root_committee()]
+    }
+
+    fn leader(&self, view: View) -> NodeId {
+        self.nodes[view as usize % self.nodes.len()]
+    }
+
+    fn super_majority_threshold(&self, _id: NodeId) -> usize {
+        0
+    }
+
+    fn leader_super_majority_threshold(&self, _id: NodeId) -> usize {
+        self.nodes.len() * 2 / 3 + 1
+    }
+}
