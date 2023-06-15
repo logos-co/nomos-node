560d6c64360d0d9b8a69cc0d4a750044fd29e097
al8n <scygliu1@gmail.com>
2023-06-15 18:24:03 +0800

    seperate log mod to a file

--- simulations/src/bin/app.rs
+++ simulations/src/bin/app.rs
@@ -32,6 +32,7 @@ use simulations::{
     node::carnot::CarnotNode, output_processors::OutData, runner::SimulationRunner,
     settings::SimulationSettings, util::node_id,
 };
+mod log;
 
 /// Main simulation wrapper
 /// Pipes together the cli arguments with the execution
@@ -237,54 +238,4 @@ fn main() -> anyhow::Result<()> {
         std::process::exit(1);
     }
     Ok(())
-}
-
-mod log {
-    use std::str::FromStr;
-
-    #[derive(Copy, Clone)]
-    pub(super) enum LogFormat {
-        Plain,
-        Json,
-    }
-
-    impl FromStr for LogFormat {
-        type Err = anyhow::Error;
-
-        fn from_str(s: &str) -> Result<Self, Self::Err> {
-            match s.trim().to_ascii_lowercase().as_str() {
-                "json" => Ok(LogFormat::Json),
-                "plain" => Ok(LogFormat::Plain),
-                _ => Err(anyhow::anyhow!("Unknown log format")),
-            }
-        }
-    }
-
-    pub(super) fn config_tracing(fmt: Option<LogFormat>) {
-        let filter = std::env::var("SIMULATION_LOG").unwrap_or_else(|_| "info".to_owned());
-        if let Some(LogFormat::Json) = fmt {
-            let subscriber = tracing_subscriber::fmt::fmt()
-                .without_time()
-                .with_line_number(true)
-                .with_env_filter(filter)
-                .with_file(false)
-                .with_target(true)
-                .with_ansi(true)
-                .json()
-                .finish();
-            tracing::subscriber::set_global_default(subscriber)
-                .expect("config_tracing is only called once");
-        } else {
-            let subscriber = tracing_subscriber::fmt::fmt()
-                .without_time()
-                .with_line_number(true)
-                .with_env_filter(filter)
-                .with_file(false)
-                .with_target(true)
-                .with_ansi(true)
-                .finish();
-            tracing::subscriber::set_global_default(subscriber)
-                .expect("config_tracing is only called once");
-        }
-    }
 }
