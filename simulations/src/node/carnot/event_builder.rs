1ecd467891ec5d2686c7b4b2ab5c5b0771ad92a6
danielsanchezq <sanchez.quiros.daniel@gmail.com>
2023-06-15 10:14:16 +0200

    Fix new view leader behaviour

--- simulations/src/node/carnot/event_builder.rs
+++ simulations/src/node/carnot/event_builder.rs
@@ -17,6 +17,7 @@ pub(crate) struct EventBuilder {
     leader_vote_message: Tally<VoteMsg>,
     vote_message: Tally<VoteMsg>,
     timeout_message: Tally<TimeoutMsg>,
+    leader_new_view_message: Tally<NewViewMsg>,
     new_view_message: Tally<NewViewMsg>,
     timeout_handler: TimeoutHandler,
     pub(crate) current_view: View,
@@ -28,6 +29,7 @@ impl EventBuilder {
             vote_message: Default::default(),
             leader_vote_message: Default::default(),
             timeout_message: Default::default(),
+            leader_new_view_message: Default::default(),
             new_view_message: Default::default(),
             current_view: View::default(),
             id,
@@ -179,19 +181,27 @@ impl EventBuilder {
                 }
                 CarnotMessage::NewView(msg) => {
                     let msg_view = msg.vote.view;
+                    let voter = msg.voter;
                     let timeout_qc = msg.vote.timeout_qc.clone();
-                    self.current_view = core::cmp::max(self.current_view, msg_view);
-                    // if we are the leader, then use the leader threshold, otherwise use the leaf threshold
-                    let threshold = if engine.is_next_leader() {
+                    let is_next_view_leader = engine.is_next_leader();
+                    let is_message_from_root_committee =
+                        engine.overlay().is_member_of_root_committee(voter);
+
+                    let tally = if is_message_from_root_committee {
+                        &mut self.leader_new_view_message
+                    } else {
+                        &mut self.new_view_message
+                    };
+
+                    // if the message comes from the root committee, then use the leader threshold, otherwise use the leaf threshold
+                    let threshold = if is_message_from_root_committee {
                         engine.leader_super_majority_threshold()
                     } else {
                         engine.super_majority_threshold()
                     };
 
-                    if let Some(new_views) =
-                        self.new_view_message.tally_by(msg_view, msg, threshold)
-                    {
-                        if engine.is_next_leader() {
+                    if let Some(votes) = tally.tally_by(msg_view, msg, threshold) {
+                        if is_next_view_leader && is_message_from_root_committee {
                             let high_qc = engine.high_qc();
                             events.push(Event::ProposeBlock {
                                 qc: Qc::Aggregated(AggregateQc {
@@ -199,13 +209,12 @@ impl EventBuilder {
                                     view: msg_view + 1,
                                 }),
                             });
-                            continue;
+                        } else {
+                            events.push(Event::NewView {
+                                timeout_qc,
+                                new_views: votes.into_iter().map(|v| v.vote).collect(),
+                            });
                         }
-
-                        events.push(Event::NewView {
-                            new_views: new_views.into_iter().map(|v| v.vote).collect(),
-                            timeout_qc,
-                        })
                     }
                 }
             }
