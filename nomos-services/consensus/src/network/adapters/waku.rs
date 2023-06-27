4d7dd4a1ab3fc8c82849bf65967ad671423395bc
Giacomo Pasini <g.pasini98@gmail.com>
2023-06-27 12:57:37 +0200

    Wait for waku db spurious delays
    Sometimes waku takes some time (e.g. a few seconds) before making
    a received message available through the archive query. Let's
    account for this by making repeated calls if the first one is not
    successful.

--- nomos-services/consensus/src/network/adapters/waku.rs
+++ nomos-services/consensus/src/network/adapters/waku.rs
@@ -54,15 +54,14 @@ impl WakuAdapter {
     }
 
     async fn archive_subscriber_stream(
-        &self,
+        network_relay: OutboundRelay<<NetworkService<Waku> as ServiceData>::Message>,
         content_topic: WakuContentTopic,
     ) -> Result<
         Box<dyn Stream<Item = WakuMessage> + Send + Sync + Unpin>,
         tokio::sync::oneshot::error::RecvError,
     > {
         let (sender, receiver) = tokio::sync::oneshot::channel();
-        if let Err((_, _e)) = self
-            .network_relay
+        if let Err((_, _e)) = network_relay
             .send(NetworkMsg::Process(WakuBackendMessage::ArchiveSubscribe {
                 query: StoreQuery {
                     pubsub_topic: Some(WAKU_CARNOT_PUB_SUB_TOPIC.clone()),
@@ -90,10 +89,35 @@ impl WakuAdapter {
             .message_subscriber_channel()
             .await
             .expect("live stream channel from waku network");
-        let archive_stream = self
-            .archive_subscriber_stream(content_topic.clone())
-            .await
-            .expect("archive stream from waku network");
+
+        struct InnerState {
+            first: bool,
+            topic: WakuContentTopic,
+            relay: OutboundRelay<<NetworkService<Waku> as ServiceData>::Message>,
+        }
+
+        let state = InnerState {
+            first: true,
+            topic: content_topic.clone(),
+            relay: self.network_relay.clone(),
+        };
+
+        // Sometimes waku takes a while make a message available in the archive, so we keep polling the archive until we get the message we want.
+        // This stream will generate a new archive stream every 100ms until the message is found, chaining them together.
+        // We expect this to be a rare occurrence, normal operation would result in this future being discarded before we even try to make a second request.
+        let archive_stream = futures::stream::unfold(state, |mut state| async move {
+            if !state.first {
+                tokio::time::sleep(std::time::Duration::from_millis(200)).await;
+            }
+            state.first = false;
+            Some((
+                Self::archive_subscriber_stream(state.relay.clone(), state.topic.clone())
+                    .await
+                    .expect("archive stream from waku network"),
+                state,
+            ))
+        })
+        .flatten();
 
         let live_stream = BroadcastStream::new(live_stream_channel)
             .zip(futures::stream::repeat(content_topic))
@@ -107,7 +131,7 @@ impl WakuAdapter {
                     _ => None,
                 }
             });
-        tokio_stream::StreamExt::merge(archive_stream, live_stream)
+        tokio_stream::StreamExt::merge(live_stream, archive_stream)
     }
 
     async fn broadcast(&self, bytes: Box<[u8]>, topic: WakuContentTopic) {
