ec4c32b2f107ddcc005482fa96dd896e2f7ca96c
Gusto <bacvinka@gmail.com>
2023-07-06 13:39:11 +0300

    Extract discordnotify and params

---
+++ ci/discordNotify.groovy
@@ -0,0 +1,32 @@
+def discordNotify(Map args=[:]) {
+  def opts = [
+    header: args.header ?: 'Nightly Fuzztest Passed',
+    title:  args.title  ?: "${env.JOB_NAME}#${env.BUILD_NUMBER}",
+    cred:   args.cred   ?: null,
+  ]
+  def repo = [
+    url: GIT_URL.minus('.git'),
+    branch: GIT_BRANCH.minus('origin/'),
+    commit: GIT_COMMIT.take(8),
+  ]
+  withCredentials([
+    string(
+      credentialsId: opts.cred,
+      variable: 'DISCORD_WEBHOOK',
+    ),
+  ]) {
+    discordSend(
+      link: env.BUILD_URL,
+      result: currentBuild.currentResult,
+      webhookURL: env.DISCORD_WEBHOOK,
+      title: opts.title,
+      description: """
+      ${opts.header}
+      Branch: [`${repo.branch}`](${repo.url}/commits/${repo.branch})
+      Commit: [`${repo.commit}`](${repo.url}/commit/${repo.commit})
+      """,
+    )
+  }
+}
+
+return this.discordNotify
