4fcb7a26d9db4adc76765df246811de3138b9521
Al Liu <scygliu1@gmail.com>
2023-08-03 19:52:07 +0800

    implement research overlay

--- simulations/src/node/carnot/mod.rs
+++ simulations/src/node/carnot/mod.rs
@@ -3,6 +3,7 @@
 mod event_builder;
 mod message_cache;
 pub mod messages;
+mod researsh_overlay;
 mod tally;
 mod timeout;
 
--- simulations/src/node/mod.rs
+++ simulations/src/node/mod.rs
@@ -5,7 +5,7 @@ pub mod dummy;
 pub mod dummy_streaming;
 
 // std
-use consensus_engine::View;
+use consensus_engine::{Committee, View};
 use std::{
     collections::BTreeMap,
     ops::{Deref, DerefMut},
@@ -199,5 +199,191 @@ impl NodeIdExt for NodeId {
         let mut bytes = [0u8; 32];
         bytes[..core::mem::size_of::<usize>()].copy_from_slice(&idx.to_be_bytes());
         NodeId::new(bytes)
+    }
+}
+
+pub trait ResearchNodeIdExt: NodeIdExt {
+    fn parent_index(&self) -> usize;
+}
+
+impl ResearchNodeIdExt for NodeId {
+    fn parent_index(&self) -> usize {
+        const SIZE: usize = core::mem::size_of::<usize>();
+        let x: [u8; 32] = (*self).into();
+        let mut buf = [0; SIZE];
+        buf.copy_from_slice(&x[32 - SIZE..]);
+        usize::from_be_bytes(buf)
+    }
+}
+
+/// Reference https://github.com/logos-co/nomos-specs/blob/Carnot-Simulation/carnot/carnot_simulation_psuedocode.py
+pub trait CommitteeExt {
+    /// Fill a binary tree of committees given the number of nodes.
+    fn fill(&mut self, num_nodes: usize, idx: usize, parent_idx: Option<usize>);
+
+    /// Returns the leftmost committee
+    fn leftmost_committee(&self) -> Option<&NodeId>;
+
+    /// Returns the rightmost committee
+    fn rightmost_committee(&self) -> Option<&NodeId>;
+
+    /// Returns the depth of the binary tree.
+    ///
+    /// level start from 0, not 1
+    fn depth(&self) -> usize;
+
+    /// Simulates the message passing from a committee to its parent committee.
+    ///
+    /// Returns the number of levels the message passed through.
+    fn simulate_message_passing(&self, lvl: usize, latency: Duration) -> usize;
+}
+
+impl CommitteeExt for Committee {
+    fn fill(&mut self, num_nodes: usize, idx: usize, parent_idx: Option<usize>) {
+        const USIZE: usize = core::mem::size_of::<usize>();
+        if num_nodes == 0 {
+            return;
+        }
+
+        let id = if let Some(parent_idx) = parent_idx {
+            let mut buf = [0; 32];
+            // use first size_of::<usize> bits to store level
+            buf[..USIZE].copy_from_slice(idx.to_be_bytes().as_ref());
+            // use last 8 bits to store parent id
+            buf[32 - USIZE..].copy_from_slice(parent_idx.to_be_bytes().as_ref());
+            NodeId::new(buf)
+        } else {
+            let mut buf = [0; 32];
+            // use first 8 bits to store level
+            buf[..8].copy_from_slice(idx.to_be_bytes().as_ref());
+            NodeId::new(buf)
+        };
+
+        self.insert(id);
+        self.fill(num_nodes - 1, idx + 1, Some(idx));
+    }
+
+    fn leftmost_committee(&self) -> Option<&NodeId> {
+        if self.is_empty() {
+            return None;
+        }
+
+        if self.len() == 1 {
+            return self.iter().next();
+        }
+
+        let depth = self.depth();
+        let min_index_at_depth = 2_usize.pow(depth as u32) - 1;
+
+        self.iter()
+            .map(|id| id.index())
+            .find(|idx| *idx >= min_index_at_depth)
+            .map(|idx| self.iter().find(|id| id.index() == idx).unwrap())
+    }
+
+    fn rightmost_committee(&self) -> Option<&NodeId> {
+        let idx = self.iter().map(|id| id.index()).max();
+        idx.map(|idx| self.iter().find(|id| id.index() == idx).unwrap())
+    }
+
+    fn depth(&self) -> usize {
+        let max_index = match self.iter().map(|id| id.index()).max() {
+            Some(max_node) => max_node,
+            None => return 0,
+        };
+
+        (max_index as f64).log2().floor() as usize
+    }
+
+    fn simulate_message_passing(&self, lvl: usize, latency: Duration) -> usize {
+        if lvl == 0 {
+            return 0;
+        }
+        let depth = self.depth();
+        if lvl > depth {
+            return depth - 1;
+        }
+
+        let min_index_at_lvl = 2_usize.pow(lvl as u32) - 1;
+        std::thread::sleep(latency * min_index_at_lvl as u32);
+        // for depth 5, we only need pass 4 times, so minus 1
+        min_index_at_lvl - 1
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_fill_and_depth() {
+        let mut committee = Committee::new();
+        committee.fill(5, 0, None);
+        assert_eq!(committee.depth(), 2);
+    }
+
+    #[test]
+    fn test_leftmost_committee() {
+        let mut committee = Committee::new();
+        committee.fill(5, 0, None);
+        assert_eq!(committee.leftmost_committee().unwrap().index(), 3);
+    }
+
+    #[test]
+    fn test_rightmost_committee() {
+        let mut committee = Committee::new();
+        committee.fill(5, 0, None);
+        assert_eq!(committee.rightmost_committee().unwrap().index(), 4);
+    }
+
+    #[test]
+    fn test_fill_and_depth_for_one_node() {
+        let mut committee = Committee::new();
+        committee.fill(1, 0, None);
+        assert_eq!(committee.depth(), 0);
+    }
+
+    #[test]
+    fn test_fill_and_depth_for_zero_nodes() {
+        let mut committee = Committee::new();
+        committee.fill(0, 0, None);
+        assert_eq!(committee.depth(), 0);
+    }
+
+    #[test]
+    fn test_leftmost_committee_for_one_node() {
+        let mut committee = Committee::new();
+        committee.fill(1, 0, None);
+        assert_eq!(committee.leftmost_committee().unwrap().index(), 0);
+    }
+
+    #[test]
+    fn test_rightmost_committee_for_one_node() {
+        let mut committee = Committee::new();
+        committee.fill(1, 0, None);
+        assert_eq!(committee.rightmost_committee().unwrap().index(), 0);
+    }
+
+    #[test]
+    fn test_leftmost_committee_for_zero_nodes() {
+        let committee = Committee::new();
+        assert!(committee.leftmost_committee().is_none());
+    }
+
+    #[test]
+    fn test_rightmost_committee_for_zero_nodes() {
+        let committee = Committee::new();
+        assert!(committee.rightmost_committee().is_none());
+    }
+
+    #[test]
+    fn test_simulate_msg_passing() {
+        let mut committee = Committee::new();
+        committee.fill(15, 0, None); // lvl 3
+        assert_eq!(committee.depth(), 3);
+        assert_eq!(
+            committee.simulate_message_passing(2, Duration::from_millis(1)),
+            2
+        );
     }
 }
