1109b4725252d2a4e1b9e6e1fe6bebff36384ea5
Al Liu <scygliu1@gmail.com>
2023-08-16 22:07:01 +0800

    fix PR comment

--- consensus-engine/src/util.rs
+++ consensus-engine/src/util.rs
@@ -29,55 +29,48 @@ pub(crate) mod serde_array32 {
         D: serde::Deserializer<'de>,
     {
         if deserializer.is_human_readable() {
-            <&str>::deserialize(deserializer).and_then(try_from_str::<D>)
+            <&str>::deserialize(deserializer).and_then(|s| {
+                super::parse_hex_from_str::<32>(s).map_err(<D::Error as serde::de::Error>::custom)
+            })
         } else {
             let x = <&[u8]>::deserialize(deserializer)?;
             <[u8; 32]>::try_from(x).map_err(<D::Error as serde::de::Error>::custom)
         }
     }
+}
 
-    fn try_from_str<'de, D>(mut s: &str) -> Result<[u8; 32], D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        #[derive(Debug, thiserror::Error)]
-        enum DecodeError {
-            #[error("expected str of length {expected} but got length {actual}")]
-            UnexpectedSize { expected: usize, actual: usize },
-            #[error("invalid character pair '{0}{1}'")]
-            InvalidCharacter(char, char),
-        }
+#[derive(Debug, thiserror::Error)]
+pub enum DecodeError {
+    #[error("expected str of length {expected} but got length {actual}")]
+    UnexpectedSize { expected: usize, actual: usize },
+    #[error("invalid character pair '{0}{1}'")]
+    InvalidCharacter(char, char),
+}
 
-        // check if we start with 0x or not
-        let prefix_len = if s.starts_with("0x") || s.starts_with("0X") {
-            s = &s[2..];
-            2
-        } else {
-            0
-        };
+pub(crate) fn parse_hex_from_str<const N: usize>(mut s: &str) -> Result<[u8; N], DecodeError> {
+    // check if we start with 0x or not
+    let prefix_len = if s.starts_with("0x") || s.starts_with("0X") {
+        s = &s[2..];
+        2
+    } else {
+        0
+    };
 
-        if s.len() != 32 * 2 {
-            return Err(<D::Error as serde::de::Error>::custom(
-                DecodeError::UnexpectedSize {
-                    expected: 32 * 2 + prefix_len,
-                    actual: s.len(),
-                },
-            ));
-        }
-
-        let mut output = [0; 32];
-
-        for (chars, byte) in s.as_bytes().chunks_exact(2).zip(output.iter_mut()) {
-            let (l, r) = (chars[0] as char, chars[1] as char);
-            match (l.to_digit(16), r.to_digit(16)) {
-                (Some(l), Some(r)) => *byte = (l as u8) << 4 | r as u8,
-                (_, _) => {
-                    return Err(<D::Error as serde::de::Error>::custom(
-                        DecodeError::InvalidCharacter(l, r),
-                    ))
-                }
-            };
-        }
-        Ok(output)
+    if s.len() != N * 2 {
+        return Err(DecodeError::UnexpectedSize {
+            expected: N * 2 + prefix_len,
+            actual: s.len(),
+        });
     }
+
+    let mut output = [0; N];
+
+    for (chars, byte) in s.as_bytes().chunks_exact(2).zip(output.iter_mut()) {
+        let (l, r) = (chars[0] as char, chars[1] as char);
+        match (l.to_digit(16), r.to_digit(16)) {
+            (Some(l), Some(r)) => *byte = (l as u8) << 4 | r as u8,
+            (_, _) => return Err(DecodeError::InvalidCharacter(l, r)),
+        };
+    }
+    Ok(output)
 }
