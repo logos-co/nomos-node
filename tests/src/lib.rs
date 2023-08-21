77dc749de70d1e527e765004cf0930945e60acd7
Youngjoon Lee <taxihighway@gmail.com>
2023-08-21 22:16:48 +0900

    fix the version issue of rand

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -11,7 +11,7 @@ use fraction::Fraction;
 use rand::{thread_rng, Rng};
 
 pub fn get_available_port() -> u16 {
-    let mut port: u16 = thread_rng().gen_range(8000..10000);
+    let mut port: u16 = thread_rng().gen_range(8000, 10000);
     while TcpListener::bind(("127.0.0.1", port)).is_err() {
         port += 1;
     }
