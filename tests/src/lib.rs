63df88087b574b0f1cab4a5f52e6f4dc2cdc4e43
Youngjoon Lee <taxihighway@gmail.com>
2023-08-21 22:03:51 +0900

    use thread_rng

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -4,20 +4,14 @@ pub use nodes::NomosNode;
 // std
 use std::fmt::Debug;
 use std::net::TcpListener;
-use std::sync::Mutex;
 use std::time::Duration;
 
 //crates
 use fraction::Fraction;
-use once_cell::sync::Lazy;
-use rand::{Rng, SeedableRng};
-use rand_xoshiro::Xoshiro256PlusPlus;
-
-static RNG: Lazy<Mutex<Xoshiro256PlusPlus>> =
-    Lazy::new(|| Mutex::new(Xoshiro256PlusPlus::seed_from_u64(42)));
+use rand::{thread_rng, Rng};
 
 pub fn get_available_port() -> u16 {
-    let mut port: u16 = RNG.lock().unwrap().gen_range(8000..10000);
+    let mut port: u16 = thread_rng().gen_range(8000..10000);
     while TcpListener::bind(("127.0.0.1", port)).is_err() {
         port += 1;
     }
