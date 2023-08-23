03b14f624c6a10e9918f7dde02733c075b491578
Youngjoon Lee <taxihighway@gmail.com>
2023-08-23 10:52:36 +0900

    separate roles into two streams

--- mixnet/client/src/receiver.rs
+++ mixnet/client/src/receiver.rs
@@ -1,6 +1,6 @@
 use std::{error::Error, net::SocketAddr};
 
-use futures::{stream, Stream, TryStreamExt};
+use futures::{stream, Stream, StreamExt};
 use mixnet_protocol::Body;
 use nym_sphinx::{
     chunking::{fragment::Fragment, reconstruction::MessageReconstructor},
@@ -19,57 +19,88 @@ impl Receiver {
     {
         let socket = TcpStream::connect(node_address).await?;
 
-        // MessageReconstructor buffers all received fragments (payloads)
+        Ok(Self::message_stream(Box::pin(Self::fragment_stream(
+            socket,
+        ))))
+    }
+
+    fn fragment_stream(
+        socket: TcpStream,
+    ) -> impl Stream<Item = Result<Fragment, Box<dyn Error>>> + Send + 'static {
+        stream::unfold(socket, |mut socket| async move {
+            let Ok(body) = Body::read(&mut socket).await else {
+                // TODO: Maybe this is a hard error and the stream is corrupted? In that case stop the stream
+                return Some((Err("Could not read body from socket".into()), socket));
+            };
+
+            match body {
+                Body::SphinxPacket(_) => {
+                    Some((Err("received sphinx packet not expected".into()), socket))
+                }
+                Body::FinalPayload(payload) => Some((Self::fragment_from_payload(payload), socket)),
+            }
+        })
+    }
+
+    fn message_stream(
+        fragment_stream: impl Stream<Item = Result<Fragment, Box<dyn Error>>> + Send + Unpin + 'static,
+    ) -> impl Stream<Item = Result<Vec<u8>, Box<dyn Error>>> + Send + 'static {
+        // MessageReconstructor buffers all received fragments
         // and eventually returns reconstructed messages.
         let message_reconstructor: MessageReconstructor = Default::default();
 
-        Ok(stream::unfold(
-            (socket, message_reconstructor),
-            |(mut socket, mut message_reconstructor)| async move {
-                loop {
-                    let Ok(body) = Body::read(&mut socket).await else {
-                        // TODO: Maybe this is a hard error and the stream is corrupted? In that case stop the stream
-                        return Some((Err("Could not read body from socket".into()), (socket, message_reconstructor)));
-                    };
-                    match body {
-                        Body::SphinxPacket(_) => {
-                            return Some((Err("received sphinx packet not expected".into()), (socket, message_reconstructor)));
-                        }
-                        Body::FinalPayload(payload) => {
-                            match Self::handle_payload(payload, &mut message_reconstructor).await {
-                                Ok(Some(reconstructed_message)) => {
-                                    return Some((Ok(reconstructed_message), (socket, message_reconstructor)));
-                                },
-                                Ok(None) => {
-                                    // A payload has been received but the message isn't yet reconstructed completely.
-                                    // Read more payloads.
-                                    continue;
-                                }
-                                Err(e) => {
-                                    return Some((Err(format!("Could not handle payload: {e}").into()), (socket, message_reconstructor)));
-                                },
-                            }
-                        }
-                    }
-                }
+        stream::unfold(
+            (fragment_stream, message_reconstructor),
+            |(mut fragment_stream, mut message_reconstructor)| async move {
+                let result =
+                    Self::reconstruct_message(&mut fragment_stream, &mut message_reconstructor)
+                        .await;
+                Some((result, (fragment_stream, message_reconstructor)))
             },
         )
-        .into_stream())
     }
 
-    async fn handle_payload(
-        payload: Payload,
-        message_reconstructor: &mut MessageReconstructor,
-    ) -> Result<Option<Vec<u8>>, Box<dyn Error>> {
-        let fragment = Fragment::try_from_bytes(&payload.recover_plaintext()?)?;
+    fn fragment_from_payload(payload: Payload) -> Result<Fragment, Box<dyn Error>> {
+        Ok(Fragment::try_from_bytes(&payload.recover_plaintext()?)?)
+    }
 
+    async fn reconstruct_message(
+        fragment_stream: &mut (impl Stream<Item = Result<Fragment, Box<dyn Error>>>
+                  + Send
+                  + Unpin
+                  + 'static),
+        message_reconstructor: &mut MessageReconstructor,
+    ) -> Result<Vec<u8>, Box<dyn Error>> {
+        // Read fragments until at least one message is fully reconstructed.
+        while let Some(next) = fragment_stream.next().await {
+            match next {
+                Ok(fragment) => {
+                    if let Some(message) =
+                        Self::try_reconstruct_message(fragment, message_reconstructor)
+                    {
+                        return Ok(message);
+                    }
+                }
+                Err(e) => {
+                    return Err(e);
+                }
+            }
+        }
+
+        Err("fragment_stream closed before messages are fully reconstructed".into())
+    }
+
+    fn try_reconstruct_message(
+        fragment: Fragment,
+        message_reconstructor: &mut MessageReconstructor,
+    ) -> Option<Vec<u8>> {
         let reconstruction_result = message_reconstructor.insert_new_fragment(fragment);
         match reconstruction_result {
             Some((padded_message, _)) => {
-                tracing::debug!("a reconstructed message reconstructed");
-                Ok(Some(Self::remove_padding(padded_message)?))
+                let message = Self::remove_padding(padded_message).unwrap();
+                Some(message)
             }
-            None => Ok(None),
+            None => None,
         }
     }
 
