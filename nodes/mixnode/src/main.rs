33a482369bec0c344c8a69b7e8cdb286048ec1dc
Daniel Sanchez <sanchez.quiros.daniel@gmail.com>
2023-08-30 10:29:22 +0200

    Overwatch mixnet node (#339)
    * Add mixnet service and overwatch app
    
    * remove #[tokio::main]
    
    ---------
    
    Co-authored-by: Youngjoon Lee <taxihighway@gmail.com>

--- nodes/mixnode/src/main.rs
+++ nodes/mixnode/src/main.rs
@@ -1,6 +1,14 @@
+mod services;
+
 use clap::Parser;
 use color_eyre::eyre::Result;
-use mixnet_node::{MixnetNode, MixnetNodeConfig};
+use overwatch_derive::Services;
+use overwatch_rs::overwatch::OverwatchRunner;
+use overwatch_rs::services::handle::ServiceHandle;
+use overwatch_rs::services::ServiceData;
+use overwatch_rs::DynError;
+use serde::{Deserialize, Serialize};
+use services::mixnet::MixnetNodeService;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -9,8 +17,18 @@ struct Args {
     config: std::path::PathBuf,
 }
 
-#[tokio::main]
-async fn main() -> Result<()> {
+#[derive(Deserialize, Debug, Clone, Serialize)]
+pub struct Config {
+    // pub log: <Logger as ServiceData>::Settings,
+    pub mixnode: <MixnetNodeService as ServiceData>::Settings,
+}
+
+#[derive(Services)]
+struct Mixnetnode {
+    node: ServiceHandle<MixnetNodeService>,
+}
+
+fn main() -> Result<(), DynError> {
     // Construct a subscriber that prints formatted traces to stdout
     // and use that subscriber to process traces emitted after this point
     // TODO: use the log service that nomos-node uses, if necessary
@@ -18,14 +36,14 @@ async fn main() -> Result<()> {
     tracing::subscriber::set_global_default(subscriber)?;
 
     let Args { config } = Args::parse();
-    let config = serde_yaml::from_reader::<_, MixnetNodeConfig>(std::fs::File::open(config)?)?;
+    let config = serde_yaml::from_reader::<_, Config>(std::fs::File::open(config)?)?;
 
-    let node = MixnetNode::new(config);
-
-    //TODO: graceful shutdown
-    if let Err(e) = node.run().await {
-        tracing::error!("error from mixnet-node: {e}");
-    }
-
+    let app = OverwatchRunner::<Mixnetnode>::run(
+        MixnetnodeServiceSettings {
+            node: config.mixnode,
+        },
+        None,
+    )?;
+    app.wait_finished();
     Ok(())
 }
