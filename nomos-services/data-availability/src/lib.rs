8221c6683d1331beb7b176e1b6a540f0c21ca6d6
danielsanchezq <sanchez.quiros.daniel@gmail.com>
2023-09-06 10:16:03 +0200

    Pipe new blobs and internal message handling

--- nomos-services/data-availability/src/lib.rs
+++ nomos-services/data-availability/src/lib.rs
@@ -64,7 +64,7 @@ where
     B: DaBackend + Send,
     B::Settings: Clone + Send + Sync + 'static,
     B::Blob: Send,
-    N: NetworkAdapter<Blob = B::Blob> + Send,
+    N: NetworkAdapter<Blob = B::Blob> + Send + Sync,
 {
     fn init(service_state: ServiceStateHandle<Self>) -> Result<Self, DynError> {
         let network_relay = service_state.overwatch_handle.relay();
