9f9aa82be7c11f28921baf8bef7c6b32d0ac4291
danielsanchezq <sanchez.quiros.daniel@gmail.com>
2023-09-11 14:26:28 +0200

    Added initial approach for Da protocol

--- nomos-core/src/da/mod.rs
+++ nomos-core/src/da/mod.rs
@@ -1,2 +1,17 @@
+use crate::da::attestation::Attestation;
+use crate::da::blob::Blob;
+use bytes::Bytes;
+use futures::Stream;
+use serde::de::Error;
+
 pub mod attestation;
 pub mod blob;
+
+pub trait DaProtocol {
+    type Blob: Blob;
+    type Attestation: Attestation;
+
+    fn encode<T: AsRef<[u8]>>(&self, data: T) -> Box<dyn Stream<Item = Self::Blob>>;
+    fn decode<S: Stream<Item = Self::Blob>>(&self, s: S) -> Result<Bytes, Box<dyn Error>>;
+    fn validate(&self, blob: &Self::Blob, attestation: &Self::Attestation) -> bool;
+}
