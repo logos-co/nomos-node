3b90cb786cc881863b9c8475c7cf690f1bffc6d1
Daniel Sanchez <sanchez.quiros.daniel@gmail.com>
2023-09-11 12:50:25 +0200

    Da service network (#384)
    * Make da backend async
    
    * Added remove blob
    
    * Added send_blob method to network adapter trait
    
    * Added libp2p backend
    Implemented blob stream
    
    * Implement attestation stream
    
    * Implement send methods

--- nomos-services/data-availability/src/network/mod.rs
+++ nomos-services/data-availability/src/network/mod.rs
@@ -1,3 +1,5 @@
+mod adapters;
+
 // std
 // crates
 use futures::Stream;
@@ -7,13 +9,15 @@ use nomos_network::NetworkService;
 use overwatch_rs::services::relay::OutboundRelay;
 use overwatch_rs::services::ServiceData;
 use overwatch_rs::DynError;
+use serde::de::DeserializeOwned;
+use serde::Serialize;
 
 #[async_trait::async_trait]
 pub trait NetworkAdapter {
     type Backend: NetworkBackend + 'static;
 
-    type Blob: Send + Sync + 'static;
-    type Attestation: Send + Sync + 'static;
+    type Blob: Serialize + DeserializeOwned + Send + Sync + 'static;
+    type Attestation: Serialize + DeserializeOwned + Send + Sync + 'static;
 
     async fn new(
         network_relay: OutboundRelay<<NetworkService<Self::Backend> as ServiceData>::Message>,
@@ -21,5 +25,9 @@ pub trait NetworkAdapter {
 
     async fn blob_stream(&self) -> Box<dyn Stream<Item = Self::Blob> + Unpin + Send>;
 
+    async fn attestation_stream(&self) -> Box<dyn Stream<Item = Self::Attestation> + Unpin + Send>;
+
     async fn send_attestation(&self, attestation: Self::Attestation) -> Result<(), DynError>;
+
+    async fn send_blob(&self, blob: Self::Blob) -> Result<(), DynError>;
 }
