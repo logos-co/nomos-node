2baa92b11c7909425596324840f6882267d2e7fc
danielsanchezq <sanchez.quiros.daniel@gmail.com>
2023-09-11 15:25:00 +0200

    Added certificate dispersal method

--- nomos-core/src/da/mod.rs
+++ nomos-core/src/da/mod.rs
@@ -1,17 +1,25 @@
 use crate::da::attestation::Attestation;
 use crate::da::blob::Blob;
+use crate::da::certificate::Certificate;
 use bytes::Bytes;
 use futures::Stream;
 use serde::de::Error;
 
 pub mod attestation;
 pub mod blob;
+pub mod certificate;
 
 pub trait DaProtocol {
     type Blob: Blob;
     type Attestation: Attestation;
+    type Certificate: Certificate;
 
     fn encode<T: AsRef<[u8]>>(&self, data: T) -> Box<dyn Stream<Item = Self::Blob>>;
     fn decode<S: Stream<Item = Self::Blob>>(&self, s: S) -> Result<Bytes, Box<dyn Error>>;
     fn validate(&self, blob: &Self::Blob, attestation: &Self::Attestation) -> bool;
+
+    fn certificate_dispersal<S: Stream<Item = Self::Attestation>>(
+        &self,
+        attestations: S,
+    ) -> Self::Certificate;
 }
