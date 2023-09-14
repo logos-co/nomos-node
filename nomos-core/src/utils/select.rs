827732402d0c698a5e0f725ea013642ccabb961a
Daniel Sanchez <sanchez.quiros.daniel@gmail.com>
2023-09-14 10:26:20 +0200

    tx to item on generic function
    Co-authored-by: Youngjoon Lee <taxihighway@gmail.com>

--- nomos-core/src/utils/select.rs
+++ nomos-core/src/utils/select.rs
@@ -3,8 +3,8 @@ pub fn select_from_till_fill_size<'i, const SIZE: usize, T>(
     items: impl Iterator<Item = T> + 'i,
 ) -> Box<dyn Iterator<Item = T> + 'i> {
     let mut current_size = 0usize;
-    Box::new(items.take_while(move |tx: &T| {
-        current_size += measure(tx);
+    Box::new(items.take_while(move |item: &T| {
+        current_size += measure(item);
         current_size <= SIZE
     }))
 }
