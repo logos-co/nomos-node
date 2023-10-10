e1573597500c4908dfb2cb9780ba73c55d50639c
danielsanchezq <sanchez.quiros.daniel@gmail.com>
2023-10-10 11:45:26 +0200

    Pipe lifecycle in mempool service

--- nomos-services/mempool/src/lib.rs
+++ nomos-services/mempool/src/lib.rs
@@ -14,13 +14,15 @@ use tokio::sync::oneshot::Sender;
 use crate::network::NetworkAdapter;
 use backend::{MemPool, Status};
 use nomos_core::block::BlockId;
-use nomos_network::NetworkService;
+use nomos_network::{NetworkMsg, NetworkService};
+use overwatch_rs::services::life_cycle::LifecycleMessage;
 use overwatch_rs::services::{
     handle::ServiceStateHandle,
     relay::{OutboundRelay, Relay, RelayMessage},
     state::{NoOperator, NoState},
     ServiceCore, ServiceData, ServiceId,
 };
+use tracing::error;
 
 pub struct MempoolService<N, P, D>
 where
@@ -168,7 +170,7 @@ where
             ..
         } = self;
 
-        let network_relay: OutboundRelay<_> = network_relay
+        let mut network_relay: OutboundRelay<_> = network_relay
             .connect()
             .await
             .expect("Relay connection with NetworkService should succeed");
@@ -180,69 +182,124 @@ where
         let adapter = adapter.await;
 
         let mut network_items = adapter.transactions_stream().await;
+        let mut lifecycle_stream = service_state.lifecycle_handle.message_stream();
 
         loop {
             tokio::select! {
                 Some(msg) = service_state.inbound_relay.recv() => {
-                    match msg {
-                        MempoolMsg::Add { item, key, reply_channel } => {
-                            match pool.add_item(key, item.clone()) {
-                                Ok(_id) => {
-                                    // Broadcast the item to the network
-                                    let net = network_relay.clone();
-                                    let settings = service_state.settings_reader.get_updated_settings().network;
-                                    // move sending to a new task so local operations can complete in the meantime
-                                    tokio::spawn(async move {
-                                        let adapter = N::new(settings, net).await;
-                                        adapter.send(item).await;
-                                    });
-                                    if let Err(e) = reply_channel.send(Ok(())) {
-                                        tracing::debug!("Failed to send reply to AddTx: {:?}", e);
-                                    }
-                                }
-                                Err(e) => {
-                                   tracing::debug!("could not add tx to the pool due to: {}", e);
-                                }
-                            }
-                        }
-                        MempoolMsg::View { ancestor_hint, reply_channel } => {
-                            reply_channel.send(pool.view(ancestor_hint)).unwrap_or_else(|_| {
-                                tracing::debug!("could not send back pool view")
-                            });
-                        }
-                        MempoolMsg::MarkInBlock { ids, block } => {
-                            pool.mark_in_block(&ids, block);
-                        }
-                        #[cfg(test)]
-                        MempoolMsg::BlockItems { block, reply_channel } => {
-                            reply_channel.send(pool.block_items(block)).unwrap_or_else(|_| {
-                                tracing::debug!("could not send back block items")
-                            });
-                        }
-                        MempoolMsg::Prune { ids } => { pool.prune(&ids); },
-                        MempoolMsg::Metrics { reply_channel } => {
-                            let metrics = MempoolMetrics {
-                                pending_items: pool.pending_item_count(),
-                                last_item_timestamp: pool.last_item_timestamp(),
-                            };
-                            reply_channel.send(metrics).unwrap_or_else(|_| {
-                                tracing::debug!("could not send back mempool metrics")
-                            });
-                        }
-                        MempoolMsg::Status {
-                            items, reply_channel
-                        } => {
-                            reply_channel.send(pool.status(&items)).unwrap_or_else(|_| {
-                                tracing::debug!("could not send back mempool status")
-                            });
-                        }
-                    }
+                    Self::handle_mempool_message(msg, &mut pool, &mut network_relay, &mut service_state).await;
                 }
                 Some((key, item )) = network_items.next() => {
                     pool.add_item(key, item).unwrap_or_else(|e| {
                         tracing::debug!("could not add item to the pool due to: {}", e)
                     });
                 }
+                Some(msg) = lifecycle_stream.next() =>  {
+                    if Self::handle_lifecycle_message(msg).await {
+                        break;
+                    }
+                }
+            }
+        }
+        Ok(())
+    }
+}
+
+impl<N, P, D> MempoolService<N, P, D>
+where
+    P: MemPool + Send + 'static,
+    P::Settings: Clone + Send + Sync + 'static,
+    N::Settings: Clone + Send + Sync + 'static,
+    P::Item: Clone + Debug + Send + Sync + 'static,
+    P::Key: Debug + Send + Sync + 'static,
+    N: NetworkAdapter<Item = P::Item, Key = P::Key> + Send + Sync + 'static,
+    D: Discriminant + Send,
+{
+    async fn handle_lifecycle_message(message: LifecycleMessage) -> bool {
+        match message {
+            LifecycleMessage::Shutdown(sender) => {
+                if sender.send(()).is_err() {
+                    error!(
+                        "Error sending successful shutdown signal from service {}",
+                        Self::SERVICE_ID
+                    );
+                }
+                true
+            }
+            LifecycleMessage::Kill => true,
+        }
+    }
+
+    async fn handle_mempool_message(
+        message: MempoolMsg<P::Item, P::Key>,
+        pool: &mut P,
+        network_relay: &mut OutboundRelay<NetworkMsg<N::Backend>>,
+        service_state: &mut ServiceStateHandle<Self>,
+    ) {
+        match message {
+            MempoolMsg::Add {
+                item,
+                key,
+                reply_channel,
+            } => {
+                match pool.add_item(key, item.clone()) {
+                    Ok(_id) => {
+                        // Broadcast the item to the network
+                        let net = network_relay.clone();
+                        let settings = service_state.settings_reader.get_updated_settings().network;
+                        // move sending to a new task so local operations can complete in the meantime
+                        tokio::spawn(async move {
+                            let adapter = N::new(settings, net).await;
+                            adapter.send(item).await;
+                        });
+                        if let Err(e) = reply_channel.send(Ok(())) {
+                            tracing::debug!("Failed to send reply to AddTx: {:?}", e);
+                        }
+                    }
+                    Err(e) => {
+                        tracing::debug!("could not add tx to the pool due to: {}", e);
+                    }
+                }
+            }
+            MempoolMsg::View {
+                ancestor_hint,
+                reply_channel,
+            } => {
+                reply_channel
+                    .send(pool.view(ancestor_hint))
+                    .unwrap_or_else(|_| tracing::debug!("could not send back pool view"));
+            }
+            MempoolMsg::MarkInBlock { ids, block } => {
+                pool.mark_in_block(&ids, block);
+            }
+            #[cfg(test)]
+            MempoolMsg::BlockItems {
+                block,
+                reply_channel,
+            } => {
+                reply_channel
+                    .send(pool.block_items(block))
+                    .unwrap_or_else(|_| tracing::debug!("could not send back block items"));
+            }
+            MempoolMsg::Prune { ids } => {
+                pool.prune(&ids);
+            }
+            MempoolMsg::Metrics { reply_channel } => {
+                let metrics = MempoolMetrics {
+                    pending_items: pool.pending_item_count(),
+                    last_item_timestamp: pool.last_item_timestamp(),
+                };
+                reply_channel
+                    .send(metrics)
+                    .unwrap_or_else(|_| tracing::debug!("could not send back mempool metrics"));
+            }
+            MempoolMsg::Status {
+                items,
+                reply_channel,
+            } => {
+                reply_channel
+                    .send(pool.status(&items))
+                    .unwrap_or_else(|_| tracing::debug!("could not send back mempool status"));
             }
         }
     }
