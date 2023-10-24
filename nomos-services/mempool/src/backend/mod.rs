69093ac4db0084bc7ea66e69673db36107528c05
Al Liu <scygliu1@gmail.com>
2023-10-24 12:45:04 +0800

    Cleanup example

--- nomos-services/mempool/src/backend/mod.rs
+++ nomos-services/mempool/src/backend/mod.rs
@@ -64,7 +64,7 @@ pub enum Status {
     #[cfg_attr(
         feature = "openapi",
         schema(
-            example = "e.g. 0x"
+            example = "e.g. 0x000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f"
         )
     )]
     InBlock { block: BlockId },
