96820a465ab09f94bd51634d8d33523eeec714e8
Gusto <bacvinka@gmail.com>
2023-10-30 11:17:52 +0200

    Same consensus node list for all nodes

--- testnet/scripts/configure_node.sh
+++ testnet/scripts/configure_node.sh
@@ -2,8 +2,26 @@
 
 set -e
 
+node_key_from_id() {
+	echo "${LIBP2P_NODE_MASK}" | sed "s/.\{${#NODE_ID}\}$/${NODE_ID}/"
+}
+
+# This node id will be used to generate consensus node list.
+NODE_ID=0
+
+# All nodes spawned nodes should added to consensus configuration.
+for i in $(seq 1 $LIBP2P_REPLICAS); do
+	NODE_ID=$((NODE_ID + 1))
+	NODE_KEY=$(node_key_from_id)
+	if [ -z "$OVERLAY_NODES" ]; then
+		OVERLAY_NODES=$NODE_KEY
+	else
+		OVERLAY_NODES="${OVERLAY_NODES},${NODE_KEY}"
+	fi
+done
+
 # Set env variables for nomos-node.
-NET_NODE_KEY=$(./etc/nomos/register_node.sh)
+NET_NODE_KEY=$(./etc/nomos/scripts/register_node.sh)
 CONSENSUS_PRIV_KEY=$NET_NODE_KEY
 
 node_ids=$(etcdctl get "/node/" --prefix --keys-only)
@@ -13,10 +31,8 @@ for node_id in $node_ids; do
 	node_multiaddr="/ip4/${node_ip}/tcp/3000"
 
 	if [ -z "$OVERLAY_NODES" ]; then
-		OVERLAY_NODES=$node_key
 		NET_INITIAL_PEERS=$node_multiaddr
 	else
-		OVERLAY_NODES="${OVERLAY_NODES},${node_key}"
 		NET_INITIAL_PEERS="${NET_INITIAL_PEERS},${node_multiaddr}"
 	fi
 done
