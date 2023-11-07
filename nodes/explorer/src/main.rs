0f48c8600744216265f5e778fd68e88a37806f1a
danielsanchezq <sanchez.quiros.daniel@gmail.com>
2023-11-07 18:38:49 +0100

    Add block explorer binary

---
+++ nodes/explorer/src/main.rs
@@ -0,0 +1,16 @@
+use overwatch_rs::services::handle::ServiceHandle;
+use overwatch_rs::Services;
+
+use nomos_log::Logger;
+use nomos_node::Wire;
+use nomos_storage::{backends::sled::SledBackend, StorageService};
+
+#[derive(Services)]
+struct Explorer {
+    log: ServiceHandle<Logger>,
+    storage: ServiceHandle<StorageService<SledBackend<Wire>>>,
+}
+
+fn main() {
+    println!("Hello, world!");
+}
