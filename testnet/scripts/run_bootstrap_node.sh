5c7e0f511fa1c7dbc095109be741c55aa11c03ad
gusto <bacv@users.noreply.github.com>
2023-11-09 12:59:06 +0200

    Update env and config files for testnet (#520)

--- testnet/scripts/run_bootstrap_node.sh
+++ testnet/scripts/run_bootstrap_node.sh
@@ -3,18 +3,18 @@
 set -e
 
 CONSENSUS_PRIV_KEY=$BOOTSTRAP_NODE_KEY
-DA_VOTER_KEY=$BOOTSTRAP_NODE_KEY
+DA_VOTER=$BOOTSTRAP_NODE_KEY
 NET_NODE_KEY=$BOOTSTRAP_NODE_KEY
 OVERLAY_NODES=$(/etc/nomos/scripts/consensus_node_list.sh)
 
 export CONSENSUS_PRIV_KEY \
-       DA_VOTER_KEY \
+       DA_VOTER \
        OVERLAY_NODES \
        NET_NODE_KEY
 
 echo "I am a container ${HOSTNAME} node ${NET_NODE_KEY}"
 echo "CONSENSUS_PRIV_KEY: ${CONSENSUS_PRIV_KEY}"
-echo "DA_VOTER_KEY: ${DA_VOTER_KEY}"
+echo "DA_VOTER: ${DA_VOTER}"
 echo "OVERLAY_NODES: ${OVERLAY_NODES}"
 
 exec /usr/bin/nomos-node /etc/nomos/bootstrap_config.yaml
