d0ce846a3dcc63b6c5293e28a1aaeb6ffd7b38dc
Gusto <bacvinka@gmail.com>
2024-01-29 16:47:11 +0200

    Add cli_config and volume

--- testnet/scripts/run_nomos_bot.sh
+++ testnet/scripts/run_nomos_bot.sh
@@ -4,6 +4,6 @@ echo "I am a container ${HOSTNAME} bot"
 
 while true
 do
-    /usr/bin/nomos-cli chat --author nomos-ghost --message "$(date +%H:%M:%S) ~ ping" --network-config /etc/nomos/cli_config.yml --node-addr http://bootstrap:18080
+    /usr/bin/nomos-cli chat --author nomos-ghost --message "$(date +%H:%M:%S) ~ ping" --network-config /etc/nomos/cli_config.yaml --node http://bootstrap:18080
     sleep 10
 done
