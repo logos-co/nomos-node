0c2aa4b7c9c2cb697714f933710fdf32ec05e74c
Al Liu <scygliu1@gmail.com>
2024-01-29 15:13:37 +0800

    support depth

--- nodes/explorer/src/api_backend/store.rs
+++ nodes/explorer/src/api_backend/store.rs
@@ -5,12 +5,14 @@ use std::hash::Hash;
 // crates
 use axum::extract::{Query, State};
 use axum::response::{IntoResponse, Response};
+use either::Either;
 use hyper::StatusCode;
 use serde::de::DeserializeOwned;
 use serde::{Deserialize, Serialize};
 // internal
+use full_replication::Certificate;
 use nomos_api::http::storage;
-use nomos_core::block::BlockId;
+use nomos_core::block::{Block, BlockId};
 use nomos_core::tx::Transaction;
 use nomos_node::make_request_and_return_response;
 use nomos_storage::backends::StorageSerde;
@@ -46,10 +48,17 @@ where
     make_request_and_return_response!(futures::future::try_join_all(results))
 }
 
+type Depth = usize;
+
 #[derive(Deserialize)]
 pub(crate) struct BlocksQueryParams {
     from: BlockId,
-    to: Option<BlockId>,
+    #[serde(default = "default_to")]
+    to: Either<BlockId, Depth>,
+}
+
+fn default_to() -> Either<BlockId, Depth> {
+    Either::Right(500)
 }
 
 pub(crate) async fn blocks<Tx, S>(
@@ -76,49 +85,93 @@ where
         Ok(from) => match from {
             Some(from) => from,
             None => {
-                return IntoResponse::into_response((
-                    StatusCode::NOT_FOUND,
-                    "from block not found",
-                ))
+                return IntoResponse::into_response((StatusCode::NOT_FOUND, "from block not found"))
             }
         },
         Err(e) => {
-            return IntoResponse::into_response((
-                StatusCode::INTERNAL_SERVER_ERROR,
-                e.to_string(),
-            ))
+            return IntoResponse::into_response((StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))
         }
     };
 
     // check if to is valid
-    let to = match to {
-        Some(to) => match storage::block_req::<S, Tx>(&store, to).await {
+    match to {
+        Either::Left(to) => match storage::block_req::<S, Tx>(&store, to).await {
             Ok(to) => match to {
-                Some(to) => Some(to),
-                None => {
-                    return IntoResponse::into_response((
-                        StatusCode::NOT_FOUND,
-                        "to block not found",
-                    ))
-                }
+                Some(to) => handle_to::<S, Tx>(from, to).await,
+                None => IntoResponse::into_response((StatusCode::NOT_FOUND, "to block not found")),
             },
+            Err(e) => {
+                IntoResponse::into_response((StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))
+            }
+        },
+        Either::Right(depth) => handle_depth::<S, Tx>(from, depth).await,
+    }
+}
+
+async fn handle_depth<S, Tx>(from: Block<Tx, Certificate>, depth: usize) -> Response
+where
+    Tx: Transaction
+        + Clone
+        + Debug
+        + Eq
+        + Hash
+        + Serialize
+        + DeserializeOwned
+        + Send
+        + Sync
+        + 'static,
+    <Tx as Transaction>::Hash: std::cmp::Ord + Debug + Send + Sync + 'static,
+    S: StorageSerde + Send + Sync + 'static,
+{
+    let mut cur = Some(from.header().parent());
+    let mut blocks = Vec::new();
+    while let Some(id) = cur
+        && blocks.len() < depth
+    {
+        let block = match storage::block_req::<S, Tx>(&store, id).await {
+            Ok(block) => block,
             Err(e) => {
                 return IntoResponse::into_response((
                     StatusCode::INTERNAL_SERVER_ERROR,
                     e.to_string(),
                 ))
             }
-        },
-        None => None,
-    };
+        };
 
+        match block {
+            Some(block) => {
+                cur = Some(block.header().parent());
+                blocks.push(block);
+            }
+            None => {
+                cur = None;
+            }
+        }
+    }
+
+    IntoResponse::into_response((StatusCode::OK, ::axum::Json(blocks)))
+}
+
+async fn handle_to<S, Tx>(from: Block<Tx, Certificate>, to: Block<Tx, Certificate>) -> Response
+where
+    Tx: Transaction
+        + Clone
+        + Debug
+        + Eq
+        + Hash
+        + Serialize
+        + DeserializeOwned
+        + Send
+        + Sync
+        + 'static,
+    <Tx as Transaction>::Hash: std::cmp::Ord + Debug + Send + Sync + 'static,
+    S: StorageSerde + Send + Sync + 'static,
+{
     let mut cur = Some(from.header().parent());
     let mut blocks = Vec::new();
     while let Some(id) = cur {
-        if let Some(to) = to {
-            if id == to.header().id {
-                break;
-            }
+        if id == to.header().id {
+            break;
         }
 
         let block = match storage::block_req::<S, Tx>(&store, id).await {
@@ -142,5 +195,5 @@ where
         }
     }
 
-    IntoResponse::into_response((::hyper::StatusCode::OK, ::axum::Json(blocks)))
+    IntoResponse::into_response((StatusCode::OK, ::axum::Json(blocks)))
 }
