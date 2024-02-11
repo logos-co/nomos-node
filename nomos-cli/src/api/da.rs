4bb88d7b97f53086c75e5473b1b88157363b97bf
Al Liu <scygliu1@gmail.com>
2024-02-11 21:11:07 +0800

    use explorer to query blocks

--- nomos-cli/src/api/da.rs
+++ nomos-cli/src/api/da.rs
@@ -4,12 +4,12 @@ use nomos_core::da::blob;
 use reqwest::Url;
 
 pub async fn get_blobs(
-    explorer: &Url,
+    node: &Url,
     ids: Vec<<Blob as blob::Blob>::Hash>,
 ) -> Result<Vec<Blob>, reqwest::Error> {
     const BLOBS_PATH: &str = "da/blobs";
     CLIENT
-        .post(explorer.join(BLOBS_PATH).unwrap())
+        .post(node.join(BLOBS_PATH).unwrap())
         .json(&ids)
         .send()
         .await?
