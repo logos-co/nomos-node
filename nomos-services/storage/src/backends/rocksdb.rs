bcd8c3d1d587fcedd72be22dce38350e31c1cf81
Al Liu <scygliu1@gmail.com>
2024-02-26 12:48:27 +0800

    create missing cf

--- nomos-services/storage/src/backends/rocksdb.rs
+++ nomos-services/storage/src/backends/rocksdb.rs
@@ -92,11 +92,13 @@ impl<SerdeOp: StorageSerde + Send + Sync + 'static> StorageBackend for RocksBack
             (false, None) => {
                 let mut opts = Options::default();
                 opts.create_if_missing(true);
+                opts.create_missing_column_families(true);
                 DB::open(&opts, db_path)?
             }
             (false, Some(cf)) => {
                 let mut opts = Options::default();
                 opts.create_if_missing(true);
+                opts.create_missing_column_families(true);
                 DB::open_cf(&opts, db_path, [cf])?
             }
         };
