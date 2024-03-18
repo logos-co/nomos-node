991ab2ebdda52f18a453740071772cb49e49d830
Youngjoon Lee <5462944+youngjoon-lee@users.noreply.github.com>
2024-03-18 11:48:25 +0900

    add comment

--- nomos-core/src/utils/asyncwritable.rs
+++ nomos-core/src/utils/asyncwritable.rs
@@ -2,6 +2,7 @@ use std::{fmt::Debug, io, pin::Pin};
 
 use futures::AsyncWrite;
 
+/// Trait for types that can be written to an `AsyncWrite` stream.
 #[async_trait::async_trait]
 pub trait AsyncWritable: Debug {
     async fn write(&self, writer: &mut Pin<Box<dyn AsyncWrite + Send>>) -> io::Result<()>;
