a933c7324541f5aaaa482606b8049a2c253edf9a
Giacomo Pasini <21265557+zeegomo@users.noreply.github.com>
2024-03-18 16:04:16 +0100

    add cryptarchia leadership (#613)

--- nomos-core/src/crypto.rs
+++ nomos-core/src/crypto.rs
@@ -4,4 +4,4 @@ pub type PublicKey = [u8; 32];
 pub type PrivateKey = [u8; 32];
 pub type Signature = [u8; 32];
 
-pub(crate) type Blake2b = blake2::Blake2b<U32>;
+pub type Blake2b = blake2::Blake2b<U32>;
--- nomos-services/cryptarchia-consensus/src/leadership.rs
+++ nomos-services/cryptarchia-consensus/src/leadership.rs
@@ -1,1 +1,22 @@
+use cryptarchia_engine::Slot;
+use cryptarchia_ledger::{Coin, Config, EpochState, LeaderProof};
 
+pub struct Leader {
+    coins: Vec<Coin>,
+    config: cryptarchia_ledger::Config,
+}
+
+impl Leader {
+    pub fn new(coins: Vec<Coin>, config: Config) -> Self {
+        Leader { coins, config }
+    }
+
+    pub fn build_proof_for(&mut self, epoch_state: &EpochState, slot: Slot) -> Option<LeaderProof> {
+        for coin in &self.coins {
+            if coin.is_slot_leader(epoch_state, slot, &self.config.consensus_config) {
+                return Some(coin.to_proof(slot));
+            }
+        }
+        None
+    }
+}
