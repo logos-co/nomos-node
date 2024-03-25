b33dbc27e801ba1082fe80e3025fd4a657d83356
danielsanchezq <sanchez.quiros.daniel@gmail.com>
2024-03-25 16:13:21 +0100

    Base cleaning of da to new traits/structure Added new da protocols and types

--- nomos-cli/src/api/da.rs
+++ nomos-cli/src/api/da.rs
@@ -1,18 +1,4 @@
 use super::CLIENT;
-use full_replication::Blob;
-use nomos_core::da::blob;
+use full_replication::Certificate;
+use nomos_core::da::certificate;
 use reqwest::Url;
-
-pub async fn get_blobs(
-    node: &Url,
-    ids: Vec<<Blob as blob::Blob>::Hash>,
-) -> Result<Vec<Blob>, reqwest::Error> {
-    const BLOBS_PATH: &str = "da/blobs";
-    CLIENT
-        .post(node.join(BLOBS_PATH).unwrap())
-        .json(&ids)
-        .send()
-        .await?
-        .json()
-        .await
-}
