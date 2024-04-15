aff3cf14316906a817ef5f1a71d796bef4a1bdbe
Daniel Sanchez Quiros <sanchez.quiros.daniel@gmail.com>
2024-04-15 19:57:23 +0200

    Implement rs encode rows test Fix related bugs Refactored API

--- nomos-da/kzgrs/src/rs.rs
+++ nomos-da/kzgrs/src/rs.rs
@@ -14,14 +14,14 @@ pub fn encode(
     polynomial: &DensePolynomial<Fr>,
     evaluations: &Evaluations<Fr>,
     factor: usize,
-    domain: &GeneralEvaluationDomain<Fr>,
+    domain: GeneralEvaluationDomain<Fr>,
 ) -> Evaluations<Fr> {
     assert!(factor > 1);
     Evaluations::from_vec_and_domain(
         (0..evaluations.evals.len() * factor)
             .map(|i| polynomial.evaluate(&domain.element(i)))
             .collect(),
-        *domain,
+        domain,
     )
 }
 
@@ -31,7 +31,7 @@ pub fn encode(
 pub fn decode(
     original_chunks_len: usize,
     points: &[Option<Fr>],
-    domain: &GeneralEvaluationDomain<Fr>,
+    domain: GeneralEvaluationDomain<Fr>,
 ) -> Evaluations<Fr> {
     let (points, roots_of_unity): (Vec<Fr>, Vec<Fr>) = points
         .iter()
@@ -49,7 +49,7 @@ pub fn decode(
         (0..original_chunks_len)
             .map(|i| coeffs.evaluate(&domain.element(i)))
             .collect(),
-        *domain,
+        domain,
     )
 }
 
