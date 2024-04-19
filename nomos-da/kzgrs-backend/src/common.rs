e344eee86eabf8467d5676930ff1e4aa4a6a09c5
Daniel Sanchez <sanchez.quiros.daniel@gmail.com>
2024-04-19 12:14:00 +0200

    Da: v1 encoder (#633)
    * Added new kzgrs core module
    
    * Implemented bytes_to_polynomial and tests
    
    * Use coefficient form
    
    * Refactor evaluations into method
    
    * Use domain elements instead of roots of unity in tests
    
    * Fix encoding and test
    
    * Clippy happy
    
    * Add comments
    
    * Implement polynomial commitment
    
    * Implement proof generation
    
    * Sketch fn signature for verification
    
    * implement proof verification
    
    * Implemented verification and tests
    
    * Return evaluations from bytes_to_polynomial as well
    
    * Use modular le bytes
    
    * Implement rs encode/decode
    
    * Implement decoding tests
    
    * Implement decode using lagrange
    
    * Added chunksmatrix
    
    * Implement encoder with chunkify
    
    * Added missing files
    
    * Implement commit row commitments
    
    * Implement compute elements (row) proofs
    
    * Fix lib and types exposures
    
    * Implement encoder main methods
    
    * Implement encode method
    
    * Implement chunkify test
    Fix related bugs
    
    * Implement compute row kzg commitments
    Fix related bugs
    
    * Implement rs encode rows test
    Fix related bugs
    Refactored API
    
    * Implement row proofs tests
    Fix fieldelement encoding/decoding bug
    
    * Implement aggregated commitment test
    Implement aggregated column proofs test
    
    * Cleanup
    
    * Fix deps
    
    * Fix tests

---
+++ nomos-da/kzgrs-backend/src/common.rs
@@ -0,0 +1,118 @@
+use blake2::digest::{Update, VariableOutput};
+use kzgrs::Commitment;
+use std::io::Cursor;
+
+#[derive(Clone, Eq, PartialEq, Debug)]
+pub struct Chunk(pub Vec<u8>);
+pub struct Row(pub Vec<Chunk>);
+pub struct Column(pub Vec<Chunk>);
+pub struct ChunksMatrix(pub Vec<Row>);
+
+impl Chunk {
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+    pub fn as_bytes(&self) -> Vec<u8> {
+        self.0.to_vec()
+    }
+
+    pub const fn empty() -> Self {
+        Self(vec![])
+    }
+}
+
+impl From<&[u8]> for Chunk {
+    fn from(value: &[u8]) -> Self {
+        Self(value.to_vec())
+    }
+}
+
+impl Row {
+    pub fn iter(&self) -> impl Iterator<Item = &Chunk> {
+        self.0.iter()
+    }
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+    pub fn as_bytes(&self) -> Vec<u8> {
+        self.0.iter().flat_map(Chunk::as_bytes).collect()
+    }
+}
+
+impl Column {
+    #[allow(unused)]
+    pub fn iter(&self) -> impl Iterator<Item = &Chunk> {
+        self.0.iter()
+    }
+    #[allow(unused)]
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+    pub fn as_bytes(&self) -> Vec<u8> {
+        self.0.iter().flat_map(Chunk::as_bytes).collect()
+    }
+}
+
+impl FromIterator<Chunk> for Row {
+    fn from_iter<T: IntoIterator<Item = Chunk>>(iter: T) -> Self {
+        Self(iter.into_iter().collect())
+    }
+}
+
+impl FromIterator<Chunk> for Column {
+    fn from_iter<T: IntoIterator<Item = Chunk>>(iter: T) -> Self {
+        Self(iter.into_iter().collect())
+    }
+}
+
+impl ChunksMatrix {
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+    pub fn rows(&self) -> impl Iterator<Item = &Row> + '_ {
+        self.0.iter()
+    }
+    pub fn columns(&self) -> impl Iterator<Item = Column> + '_ {
+        let size = self.0.first().map(|r| r.0.len()).unwrap_or(0);
+        (0..size).map(|i| {
+            self.0
+                .iter()
+                .map(|row| row.0.get(i).cloned().unwrap_or_else(Chunk::empty))
+                .collect::<Column>()
+        })
+    }
+
+    pub fn transposed(&self) -> Self {
+        Self(self.columns().map(|c| Row(c.0)).collect())
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = &Row> {
+        self.0.iter()
+    }
+}
+
+impl FromIterator<Row> for ChunksMatrix {
+    fn from_iter<T: IntoIterator<Item = Row>>(iter: T) -> Self {
+        Self(iter.into_iter().collect())
+    }
+}
+
+pub fn hash_column_and_commitment<const HASH_SIZE: usize>(
+    column: &Column,
+    commitment: &Commitment,
+) -> [u8; HASH_SIZE] {
+    use ark_serialize::CanonicalSerialize;
+    let mut hasher = blake2::Blake2bVar::new(HASH_SIZE)
+        .unwrap_or_else(|e| panic!("Blake2b should work for size {HASH_SIZE}, {e}"));
+    hasher.update(column.as_bytes().as_ref());
+    let mut buff = Cursor::new(vec![]);
+    commitment
+        .serialize_uncompressed(&mut buff)
+        .expect("Serialization of commitment should work");
+    hasher.update(buff.into_inner().as_ref());
+    hasher
+        .finalize_boxed()
+        .to_vec()
+        .try_into()
+        .unwrap_or_else(|_| panic!("Size is guaranteed by constant {HASH_SIZE:?}"))
+}
