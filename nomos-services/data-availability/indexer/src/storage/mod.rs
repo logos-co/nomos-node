86a569255ee1f9bdcb616de278b2f63067876a4b
Gusto <bacvinka@gmail.com>
2024-04-25 17:52:01 +0300

    Da storage adapter methods

--- nomos-services/data-availability/indexer/src/storage/mod.rs
+++ nomos-services/data-availability/indexer/src/storage/mod.rs
@@ -1,3 +1,6 @@
+use std::ops::Range;
+
+use nomos_core::da::certificate::{metadata::Metadata, vid::VID};
 use nomos_storage::{backends::StorageBackend, StorageService};
 use overwatch_rs::services::{relay::OutboundRelay, ServiceData};
 
@@ -6,12 +9,15 @@ pub trait DaStorageAdapter {
     type Backend: StorageBackend + Send + Sync + 'static;
 
     type Blob;
-    type VID;
+    type VID: VID;
 
     async fn new(
         storage_relay: OutboundRelay<<StorageService<Self::Backend> as ServiceData>::Message>,
     ) -> Self;
 
-    async fn add_index(&self);
-    async fn get_range(&self);
+    async fn add_index(&self, vid: Self::VID) -> bool;
+    async fn get_range(
+        &self,
+        range: Range<<Self::VID as Metadata>::Index>,
+    ) -> Vec<Option<Self::Blob>>;
 }
