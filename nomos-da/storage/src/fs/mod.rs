eb3eef364d3f1d8ed65cf712f34f767e3fe1e4ef
Gusto <bacvinka@gmail.com>
2024-05-15 00:03:41 +0300

    Use updated nomos da storage in indexer

--- nomos-da/storage/src/fs/mod.rs
+++ nomos-da/storage/src/fs/mod.rs
@@ -10,13 +10,11 @@ use tokio::{
 
 // TODO: Rocksdb has a feature called BlobDB that handles largo blob storing, but further
 // investigation needs to be done to see if rust wrapper supports it.
-pub async fn load_blob(base_dir: PathBuf, app_id: &[u8], id: &[u8]) -> Option<Bytes> {
-    let app_id = hex::encode(app_id);
-    let id = hex::encode(id);
+pub async fn load_blob(base_dir: PathBuf, blob_id: &[u8]) -> Option<Bytes> {
+    let blob_id = hex::encode(blob_id);
 
     let mut path = base_dir;
-    path.push(app_id);
-    path.push(id);
+    path.push(blob_id);
 
     let mut file = match File::open(path).await {
         Ok(file) => file,
@@ -37,16 +35,13 @@ pub async fn load_blob(base_dir: PathBuf, app_id: &[u8], id: &[u8]) -> Option<By
 
 pub async fn write_blob(
     base_dir: PathBuf,
-    app_id: &[u8],
-    id: &[u8],
+    blob_id: &[u8],
     data: &[u8],
 ) -> Result<(), std::io::Error> {
-    let app_id = hex::encode(app_id);
-    let id = hex::encode(id);
+    let blob_id = hex::encode(blob_id);
 
     let mut path = base_dir;
-    path.push(app_id);
-    path.push(id);
+    path.push(blob_id);
 
     if let Some(parent) = path.parent() {
         tokio::fs::create_dir_all(parent).await?;
--- nomos-da/storage/src/rocksdb/mod.rs
+++ nomos-da/storage/src/rocksdb/mod.rs
@@ -1,7 +1,8 @@
 use bytes::{Bytes, BytesMut};
 
 pub const DA_VID_KEY_PREFIX: &str = "da/vid/";
-pub const DA_ATTESTED_KEY_PREFIX: &str = "da/attested/";
+pub const DA_ATTESTED_BLOB_ID_KEY_PREFIX: &str = "da/attested/";
+pub const DA_BLOB_ATTESTATION_KEY_PREFIX: &str = "da/attestation/";
 
 pub fn key_bytes(prefix: &str, id: impl AsRef<[u8]>) -> Bytes {
     let mut buffer = BytesMut::new();
