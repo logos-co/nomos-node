9313ff817121f4f358383ec4cc411c60fe30b06d
Gusto <bacvinka@gmail.com>
2024-05-14 20:10:22 +0300

    Common nomos da storage crate

--- nomos-da/storage/src/fs/mod.rs
+++ nomos-da/storage/src/fs/mod.rs
@@ -10,11 +10,13 @@ use tokio::{
 
 // TODO: Rocksdb has a feature called BlobDB that handles largo blob storing, but further
 // investigation needs to be done to see if rust wrapper supports it.
-pub async fn load_blob(base_dir: PathBuf, blob_id: &[u8]) -> Option<Bytes> {
-    let blob_id = hex::encode(blob_id);
+pub async fn load_blob(base_dir: PathBuf, app_id: &[u8], id: &[u8]) -> Option<Bytes> {
+    let app_id = hex::encode(app_id);
+    let id = hex::encode(id);
 
     let mut path = base_dir;
-    path.push(blob_id);
+    path.push(app_id);
+    path.push(id);
 
     let mut file = match File::open(path).await {
         Ok(file) => file,
@@ -35,13 +37,16 @@ pub async fn load_blob(base_dir: PathBuf, blob_id: &[u8]) -> Option<Bytes> {
 
 pub async fn write_blob(
     base_dir: PathBuf,
-    blob_id: &[u8],
+    app_id: &[u8],
+    id: &[u8],
     data: &[u8],
 ) -> Result<(), std::io::Error> {
-    let blob_id = hex::encode(blob_id);
+    let app_id = hex::encode(app_id);
+    let id = hex::encode(id);
 
     let mut path = base_dir;
-    path.push(blob_id);
+    path.push(app_id);
+    path.push(id);
 
     if let Some(parent) = path.parent() {
         tokio::fs::create_dir_all(parent).await?;
