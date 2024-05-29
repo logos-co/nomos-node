c5c9152602527cb5f43f5f219b72224724f4afd1
Gusto <bacvinka@gmail.com>
2024-05-29 17:37:22 +0300

    Set coin params via env in testnet

--- testnet/scripts/run_nomos_node.sh
+++ testnet/scripts/run_nomos_node.sh
@@ -4,7 +4,9 @@ set -e
 
 # Set env variables for nomos-node.
 NET_NODE_KEY=$(/etc/nomos/scripts/register_node.sh)
-CONSENSUS_PRIV_KEY=$NET_NODE_KEY
+CONSENSUS_COIN_SK=$NET_NODE_KEY
+CONSENSUS_COIN_NONCE=$NET_NODE_KEY
+CONSENSUS_COIN_VALUE=1
 DA_VOTER=$CONSENSUS_PRIV_KEY
 OVERLAY_NODES=$(/etc/nomos/scripts/consensus_node_list.sh)
 
@@ -21,14 +23,18 @@ for node_id in $node_ids; do
 	fi
 done
 
-export CONSENSUS_PRIV_KEY \
+export CONSENSUS_COIN_SK \
+       CONSENSUS_COIN_NONCE \
+       CONSENSUS_COIN_VALUE \
        DA_VOTER \
        OVERLAY_NODES \
        NET_NODE_KEY \
        NET_INITIAL_PEERS
 
 echo "I am a container ${HOSTNAME} node ${NET_NODE_KEY}"
-echo "CONSENSUS_PRIV_KEY: ${CONSENSUS_PRIV_KEY}"
+echo "CONSENSUS_COIN_SK: ${CONSENSUS_COIN_SK}"
+echo "CONSENSUS_COIN_NONCE: ${CONSENSUS_COIN_NONCE}"
+echo "CONSENSUS_COIN_VALUE: ${CONSENSUS_COIN_VALUE}"
 echo "DA_VOTER: ${DA_VOTER}"
 echo "OVERLAY_NODES: ${OVERLAY_NODES}"
 echo "NET_INITIAL_PEERS: ${NET_INITIAL_PEERS}"
