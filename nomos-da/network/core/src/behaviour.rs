7a53110931d4549ea32af1532e729634c0b9e7c0
danielSanchezQ <3danimanimal@gmail.com>
2024-07-24 08:48:14 +0200

    Refuse connection for non members

--- nomos-da/network/core/src/behaviour.rs
+++ nomos-da/network/core/src/behaviour.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeSet;
+use std::collections::{BTreeSet, VecDeque};
 use std::task::{Context, Poll};
 
 use libp2p::{Multiaddr, PeerId};
@@ -8,58 +8,69 @@ use libp2p::swarm::{
     THandlerOutEvent, ToSwarm,
 };
 
-use crate::handler::DaNetworkHandler;
+use subnetworks_assignations::MembershipHandler;
+
+use crate::handler::{DaMessage, DaNetworkHandler, HandlerEventToBehaviour};
 
 pub type SubnetworksSize = u16;
 pub type SubnetworkId = u16;
-pub struct NomosDaNetworkBehaviour {
+
+pub enum NomosDaEvent {
+    IncomingMessage { peer_id: PeerId, message: DaMessage },
+}
+pub struct NomosDaNetworkBehaviour<Membership> {
     local_peer_id: PeerId,
     nodes: BTreeSet<PeerId>,
     subnetworks_size: SubnetworksSize,
     subscribed_subnetworks: BTreeSet<SubnetworkId>,
+    membership: Membership,
+    handler_events: VecDeque<NomosDaEvent>,
 }
 
-impl NetworkBehaviour for NomosDaNetworkBehaviour {
-    type ConnectionHandler = DaNetworkHandler;
-    type ToSwarm = ();
-
-    fn handle_pending_inbound_connection(
-        &mut self,
-        connection_id: ConnectionId,
-        _local_addr: &Multiaddr,
-        _remote_addr: &Multiaddr,
-    ) -> Result<(), ConnectionDenied> {
-        todo!()
+impl<M> NomosDaNetworkBehaviour<M>
+where
+    M: MembershipHandler<NetworkId = SubnetworkId, Id = PeerId>,
+{
+    fn is_neighbour(&self, peer_id: &PeerId) -> bool {
+        self.membership
+            .membership(&self.local_peer_id)
+            .intersection(&self.membership.membership(&peer_id))
+            .count()
+            > 0
     }
+}
+
+impl<M> NetworkBehaviour for NomosDaNetworkBehaviour<M>
+where
+    M: MembershipHandler<NetworkId = SubnetworkId, Id = PeerId> + 'static,
+{
+    type ConnectionHandler = DaNetworkHandler;
+    type ToSwarm = NomosDaEvent;
 
     fn handle_established_inbound_connection(
         &mut self,
         _connection_id: ConnectionId,
-        peer: PeerId,
-        local_addr: &Multiaddr,
-        remote_addr: &Multiaddr,
+        peer_id: PeerId,
+        _local_addr: &Multiaddr,
+        _remote_addr: &Multiaddr,
     ) -> Result<THandler<Self>, ConnectionDenied> {
-        todo!()
-    }
-
-    fn handle_pending_outbound_connection(
-        &mut self,
-        _connection_id: ConnectionId,
-        _maybe_peer: Option<PeerId>,
-        _addresses: &[Multiaddr],
-        _effective_role: Endpoint,
-    ) -> Result<Vec<Multiaddr>, ConnectionDenied> {
-        todo!()
+        if !self.is_neighbour(&peer_id) {
+            // TODO: use proper error types
+            return Err(ConnectionDenied::new(
+                "Peer is not a member of our subnetwork",
+            ));
+        }
+        Ok(DaNetworkHandler::new())
     }
 
     fn handle_established_outbound_connection(
         &mut self,
         _connection_id: ConnectionId,
-        peer: PeerId,
-        addr: &Multiaddr,
-        role_override: Endpoint,
+        _peer: PeerId,
+        _addr: &Multiaddr,
+        _role_override: Endpoint,
     ) -> Result<THandler<Self>, ConnectionDenied> {
-        todo!()
+        Ok(DaNetworkHandler::new())
     }
 
     fn on_swarm_event(&mut self, event: FromSwarm) {
@@ -68,17 +79,29 @@ impl NetworkBehaviour for NomosDaNetworkBehaviour {
 
     fn on_connection_handler_event(
         &mut self,
-        _peer_id: PeerId,
+        peer_id: PeerId,
         _connection_id: ConnectionId,
-        _event: THandlerOutEvent<Self>,
+        event: THandlerOutEvent<Self>,
     ) {
-        todo!()
+        match event {
+            HandlerEventToBehaviour::IncomingMessage { message } => {
+                self.handler_events
+                    .push_back(NomosDaEvent::IncomingMessage { message, peer_id });
+            }
+            HandlerEventToBehaviour::OutgoingMessageError { .. } => {
+                todo!("Retry or report?")
+            }
+        }
     }
 
     fn poll(
         &mut self,
         cx: &mut Context<'_>,
     ) -> Poll<ToSwarm<Self::ToSwarm, THandlerInEvent<Self>>> {
-        todo!()
+        if let Some(event) = self.handler_events.pop_back() {
+            Poll::Ready(ToSwarm::GenerateEvent(event))
+        } else {
+            Poll::Pending
+        }
     }
 }
