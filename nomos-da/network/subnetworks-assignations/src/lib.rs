b9efe1f1ba2b12d35855c09fc8af3d94a673c692
Daniel Sanchez <sanchez.quiros.daniel@gmail.com>
2024-07-25 17:03:32 +0000

    Da: network replication core (#680)
    * Add core structure
    Sketch first version of da network handler
    
    * Added assgination module
    Added basic assignation trait
    
    * Added assgination module
    Added basic assignation trait
    
    * Refuse connection for non members
    
    * Use proper protocol name
    
    * Split in broadcast/dispersal/sampling modules
    Move behaviour and handler to broadcast
    
    * Remove unused broadcast behaviour attributes
    
    * Add basic docs
    
    * Remove unused pending errors on broadcast handler
    
    * Clippy happy
    
    * Refactor Broadcast -> Replication
    
    * Pipe message sending
    
    * Clippy happy
    
    * Clippy fmt
    
    * Refactor outgoing -> outgoing_events
    
    * Pipe out replication
    
    * Include subnetworks id in dispersal and replication messages
    
    * Pipe replication messaging
    
    * fmt
    
    * Fix tests

---
+++ nomos-da/network/subnetworks-assignations/src/lib.rs
@@ -0,0 +1,12 @@
+use std::collections::HashSet;
+use std::hash::Hash;
+
+pub trait MembershipHandler {
+    type NetworkId: Eq + Hash;
+    type Id;
+    fn membership(&self, self_id: &Self::Id) -> HashSet<Self::NetworkId>;
+    fn is_member_of(&self, id: &Self::Id, network_id: &Self::NetworkId) -> bool {
+        self.membership(id).contains(network_id)
+    }
+    fn members_of(&self, network_id: &Self::NetworkId) -> HashSet<Self::Id>;
+}
