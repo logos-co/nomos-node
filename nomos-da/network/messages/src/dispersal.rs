9ff90e7d1ca0c0d5ea333196e6002f62f743049c
gusto <bacv@users.noreply.github.com>
2024-07-25 12:27:55 +0300

    DA: Network message types (#681)
    * Split message definitions for broadcast, dispersal and sampling
    
    * Expose dispersal messages via crate
    
    * Create nomos-da-messages crate
    
    * From trait implementations for messages
    
    * Rename broadcast to replication

---
+++ nomos-da/network/messages/src/dispersal.rs
@@ -0,0 +1,26 @@
+use crate::{common, impl_from_for_message};
+
+include!(concat!(env!("OUT_DIR"), "/nomos.da.v1.dispersal.rs"));
+
+impl_from_for_message!(
+    Message,
+    DispersalReq => DispersalReq,
+    DispersalRes => DispersalRes,
+    common::SessionReq => SessionReq,
+);
+
+#[cfg(test)]
+mod tests {
+    use crate::{common, dispersal};
+
+    #[test]
+    fn dispersal_message() {
+        let blob = common::Blob {
+            blob_id: vec![0; 32],
+            data: vec![1; 32],
+        };
+        let req = dispersal::DispersalReq { blob: Some(blob) };
+
+        assert_eq!(req.blob.unwrap().blob_id, vec![0; 32]);
+    }
+}
