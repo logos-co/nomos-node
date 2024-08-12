5f0707c276582db6a1ff69ba36168ce935bd4897
Daniel Sanchez <sanchez.quiros.daniel@gmail.com>
2024-08-12 09:12:08 +0200

    Da: Network dispersal (#688)
    * Fix reading messages state machine
    
    * Improve waker on behaviour
    
    * Rename test
    
    * First sketch
    
    * Implement replication
    
    * Refactor protocols names
    
    * Clippy happy
    
    * expose behaviour
    
    * Return dummy on outbound for validator behaviour
    
    * Implement behaviour, lacks streams handling
    
    * Cleanup
    
    * Implement stream handling
    
    * Add some documentation and comments
    
    * Clippy fixes after rebase
    
    * Move all neighbours to test_utils module
    
    * Executor only outgoing streams
    
    * First assault at testing behaviours
    
    * Add debug instead of traces
    
    * Added more logs
    
    * Bullshitting tests
    
    * Fix outstream handling, offers hook to send new peer streams
    
    * Fix blob dispersal handling
    
    * Refactor test, use new api
    
    * Imports cleanup
    
    * Working test
    
    * Add docs
    
    * Non overlapping test port

--- nomos-da/network/core/src/protocol.rs
+++ nomos-da/network/core/src/protocol.rs
@@ -1,3 +1,4 @@
 use libp2p::StreamProtocol;
 
-pub const PROTOCOL_NAME: StreamProtocol = StreamProtocol::new("/nomos/da/0.1.0");
+pub const REPLICATION_PROTOCOL: StreamProtocol = StreamProtocol::new("/nomos/da/0.1.0/replication");
+pub const DISPERSAL_PROTOCOL: StreamProtocol = StreamProtocol::new("/nomos/da/0.1.0/dispersal");
