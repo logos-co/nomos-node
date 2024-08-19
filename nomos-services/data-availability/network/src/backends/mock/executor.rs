0de36dc185ccf7a6fa516f322f88730017a70ec5
Gusto <bacvinka@gmail.com>
2024-08-19 11:55:16 +0300

    Dispersal backend mock for executor

--- nomos-services/data-availability/network/src/backends/mock/executor.rs
+++ nomos-services/data-availability/network/src/backends/mock/executor.rs
@@ -1,4 +1,4 @@
-use kzgrs_backend::common::blob::DaBlob;
+use kzgrs_backend::common::{blob::DaBlob, build_attestation_message};
 use overwatch_rs::{overwatch::handle::OverwatchHandle, services::state::NoState};
 use serde::{Deserialize, Serialize};
 use tokio::sync::{
@@ -21,7 +21,10 @@ pub enum EventKind {
 // Assuming that behaviour will wrap the nomos_da_message types.
 #[derive(Debug, Clone)]
 pub enum DisperseMessage {
-    DispersalSuccess,
+    DispersalSuccess {
+        blob_id: [u8; 32],
+        subnetwork_id: u32,
+    },
 }
 
 // A subset of sample protocol messages that will come over the wire.
@@ -51,9 +54,9 @@ pub enum Command {
 
 #[derive(Clone)]
 pub struct MockExecutorBackend {
-    config: MockConfig,
+    _config: MockConfig,
+    _commands_tx: mpsc::Sender<Command>,
     events_tx: broadcast::Sender<Event>,
-    commands_tx: mpsc::Sender<Command>,
 }
 
 #[async_trait::async_trait]
@@ -64,12 +67,12 @@ impl NetworkBackend for MockExecutorBackend {
     type EventKind = EventKind;
     type NetworkEvent = Event;
 
-    fn new(config: Self::Settings, _: OverwatchHandle) -> Self {
-        let (commands_tx, commands_rx) = tokio::sync::mpsc::channel(BUFFER_SIZE);
+    fn new(_config: Self::Settings, _: OverwatchHandle) -> Self {
+        let (_commands_tx, _) = tokio::sync::mpsc::channel(BUFFER_SIZE);
         let (events_tx, _) = tokio::sync::broadcast::channel(BUFFER_SIZE);
         Self {
-            config,
-            commands_tx,
+            _config,
+            _commands_tx,
             events_tx,
         }
     }
@@ -79,7 +82,19 @@ impl NetworkBackend for MockExecutorBackend {
             Command::Disperse {
                 blob,
                 subnetwork_id,
-            } => todo!(),
+            } => {
+                let blob_id = build_attestation_message(
+                    &blob.aggregated_column_commitment,
+                    &blob.rows_commitments,
+                );
+
+                let success_message = DisperseMessage::DispersalSuccess {
+                    blob_id,
+                    subnetwork_id,
+                };
+
+                let _ = self.events_tx.send(Event::Disperse(success_message));
+            }
         }
     }
 
