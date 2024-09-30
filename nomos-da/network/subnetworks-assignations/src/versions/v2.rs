2cc7bc3e47570e474f0972d2faaffca6036ff8eb
danielSanchezQ <3danimanimal@gmail.com>
2024-09-30 15:37:28 +0200

    Add members list to membership trait

--- nomos-da/network/subnetworks-assignations/src/versions/v2.rs
+++ nomos-da/network/subnetworks-assignations/src/versions/v2.rs
@@ -92,6 +92,10 @@ impl MembershipHandler for FillWithOriginalReplication {
     fn members_of(&self, network_id: &Self::NetworkId) -> HashSet<Self::Id> {
         self.assignations[*network_id as usize].clone()
     }
+
+    fn members(&self) -> HashSet<Self::Id> {
+        self.assignations.iter().flatten().copied().collect()
+    }
 }
 
 #[cfg(test)]
