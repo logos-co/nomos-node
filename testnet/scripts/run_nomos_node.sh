d7fe88f30b762799c46a9ac9b1519b0989391a70
gusto <bacv@users.noreply.github.com>
2024-10-03 14:36:36 +0300

    Remove graylog and related containers (#804)

--- testnet/scripts/run_nomos_node.sh
+++ testnet/scripts/run_nomos_node.sh
@@ -5,7 +5,8 @@ set -e
 export CFG_FILE_PATH="/config.yaml" \
        CFG_SERVER_ADDR="http://cfgsync:4400" \
        CFG_HOST_IP=$(hostname -i) \
+       LOG_LEVEL="INFO" \
        RISC0_DEV_MODE=true
 
 /usr/bin/cfgsync-client && \
-    exec /usr/bin/nomos-node /config.yaml --with-metrics --log-backend gelf --log-addr graylog:12201
+    exec /usr/bin/nomos-node /config.yaml --with-metrics
