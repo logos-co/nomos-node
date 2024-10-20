b12d0116284e5907eec04c995e36519a22af26d8
Deep Mondal <dm7041979@gmail.com>
2024-10-20 12:28:40 +0000

    rebased
    Executor in testnet (#839)
    
    Fix: membership comments to documentation (#838)
    
    * rebased
    
    * doc comment add
    
    * fmt
    
    update
    
    fmt

--- tests/src/topology/configs/mod.rs
+++ tests/src/topology/configs/mod.rs
@@ -1,8 +1,10 @@
+pub mod api;
 pub mod consensus;
 pub mod da;
 pub mod mix;
 pub mod network;
 
+use api::GeneralApiConfig;
 use consensus::GeneralConsensusConfig;
 use da::GeneralDaConfig;
 use mix::GeneralMixConfig;
@@ -10,6 +12,7 @@ use network::GeneralNetworkConfig;
 
 #[derive(Clone)]
 pub struct GeneralConfig {
+    pub api_config: GeneralApiConfig,
     pub consensus_config: GeneralConsensusConfig,
     pub da_config: GeneralDaConfig,
     pub network_config: GeneralNetworkConfig,
