0d1e75beebfcb13357205e1b51b9d3cd98d49275
Gusto <bacvinka@gmail.com>
2024-10-23 16:18:57 +0300

    Remove chat and dissemination from nomos-cli

--- nomos-cli/src/cmds/mod.rs
+++ nomos-cli/src/cmds/mod.rs
@@ -1,22 +1,10 @@
 use clap::Subcommand;
 
-// pub mod chat;
-pub mod disseminate;
-
 #[derive(Debug, Subcommand)]
-pub enum Command {
-    /// Send a blob to the network and collect attestations to create a DA proof
-    Disseminate(disseminate::Disseminate),
-    // /// (Almost) Instant messaging protocol on top of the Nomos network
-    // Chat(chat::NomosChat),
-}
+pub enum Command {}
 
 impl Command {
     pub fn run(&self) -> Result<(), Box<dyn std::error::Error>> {
-        match self {
-            Command::Disseminate(cmd) => cmd.run(),
-            // Command::Chat(cmd) => cmd.run(),
-        }?;
         Ok(())
     }
 }
