58a07835e280bd1a6ba05f18b7d1642b4092da9c
Gusto <bacvinka@gmail.com>
2024-10-25 10:32:12 +0300

    Local debug setup for integration tests

--- tests/src/topology/configs/tracing.rs
+++ tests/src/topology/configs/tracing.rs
@@ -1,14 +1,52 @@
-use nomos_tracing_service::TracingSettings;
+use nomos_tracing::{logging::loki::LokiConfig, tracing::otlp::OtlpTracingConfig};
+use nomos_tracing_service::{FilterLayer, LoggerLayer, TracingLayer, TracingSettings};
+use tracing::Level;
 
-#[derive(Clone)]
+#[derive(Clone, Default)]
 pub struct GeneralTracingConfig {
     pub tracing_settings: TracingSettings,
 }
 
+impl GeneralTracingConfig {
+    #[allow(dead_code)]
+    fn local_debug_tracing(id: usize) -> Self {
+        Self {
+            tracing_settings: TracingSettings {
+                logger: LoggerLayer::Loki(LokiConfig {
+                    endpoint: "http://localhost:3100".try_into().unwrap(),
+                    host_identifier: format!("node-{id}"),
+                }),
+                tracing: TracingLayer::Otlp(OtlpTracingConfig {
+                    endpoint: "http://localhost:4317".try_into().unwrap(),
+                    sample_ratio: 0.1,
+                }),
+                filter: FilterLayer::EnvFilter(nomos_tracing::filter::envfilter::EnvFilterConfig {
+                    // Allow events only from modules that matches the regex, if it matches - use
+                    // provided tracing level. Libp2p and risc0 related crates are very log
+                    // intensive in debug mode.
+                    filters: [("nomos", "debug")]
+                        .iter()
+                        .map(|(k, v)| (k.to_string(), v.to_string()))
+                        .collect(),
+                }),
+                level: Level::DEBUG,
+            },
+        }
+    }
+}
+
 pub fn create_tracing_configs(ids: &[[u8; 32]]) -> Vec<GeneralTracingConfig> {
     ids.iter()
-        .map(|_| GeneralTracingConfig {
-            tracing_settings: Default::default(),
+        .enumerate()
+        .map(|(_i, _)| {
+            #[cfg(feature = "debug")]
+            {
+                GeneralTracingConfig::local_debug_tracing(_i)
+            }
+            #[cfg(not(feature = "debug"))]
+            {
+                GeneralTracingConfig::default()
+            }
         })
         .collect()
 }
