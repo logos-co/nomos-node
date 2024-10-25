c580075fb899b639d09baeff99bae11aecd14e8e
Youngjoon Lee <5462944+youngjoon-lee@users.noreply.github.com>
2024-10-25 20:42:56 +0900

    Mix: Use TemporalProcessor in MessageBlend (#847)
    * Mix: Add Persistent Transmission (Tier 1)
    
    * Mix: Implement TemporalProcessor
    
    * Mix: Use TemporalProcessor in MessageBlend

--- nomos-mix/core/src/message_blend/mod.rs
+++ nomos-mix/core/src/message_blend/mod.rs
@@ -2,12 +2,13 @@ mod crypto;
 mod temporal;
 
 pub use crypto::CryptographicProcessorSettings;
+use futures::StreamExt;
 pub use temporal::TemporalProcessorSettings;
 
 use serde::{Deserialize, Serialize};
 use tokio::sync::mpsc;
 
-use crate::message_blend::crypto::CryptographicProcessor;
+use crate::message_blend::{crypto::CryptographicProcessor, temporal::TemporalProcessor};
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct MessageBlendSettings {
@@ -29,9 +30,9 @@ pub struct MessageBlend {
     outbound_message_sender: mpsc::UnboundedSender<Vec<u8>>,
     /// To release fully unwrapped messages
     fully_unwrapped_message_sender: mpsc::UnboundedSender<Vec<u8>>,
-    /// Processor
+    /// Processors
     cryptographic_processor: CryptographicProcessor,
-    // TODO: Add TemporalProcessor
+    temporal_processor: TemporalProcessor<TemporalProcessableMessage>,
 }
 
 impl MessageBlend {
@@ -48,25 +49,74 @@ impl MessageBlend {
             outbound_message_sender,
             fully_unwrapped_message_sender,
             cryptographic_processor: CryptographicProcessor::new(settings.cryptographic_processor),
-            // TODO: Initialize TemporalProcessor
+            temporal_processor: TemporalProcessor::<_>::new(settings.temporal_processor),
         }
     }
 
     pub async fn run(&mut self) {
-        // TODO: This is a mock implmementation.
-        //  The actual implementation must use TemporalProcessor.
         loop {
             tokio::select! {
                 Some(new_message) = self.new_message_receiver.recv() => {
-                    let msg = self.cryptographic_processor.wrap_message(&new_message).unwrap();
-                    self.outbound_message_sender.send(msg).unwrap();
+                    self.handle_new_message(new_message);
                 }
                 Some(incoming_message) = self.inbound_message_receiver.recv() => {
-                    let (msg, fully_unwrapped) = self.cryptographic_processor.unwrap_message(&incoming_message).unwrap();
-                    assert!(fully_unwrapped);
-                    self.fully_unwrapped_message_sender.send(msg).unwrap();
+                    self.handle_incoming_message(incoming_message);
+                }
+                Some(msg) = self.temporal_processor.next() => {
+                    self.release_temporal_processed_message(msg);
                 }
             }
         }
     }
+
+    fn handle_new_message(&mut self, message: Vec<u8>) {
+        match self.cryptographic_processor.wrap_message(&message) {
+            Ok(wrapped_message) => {
+                // Bypass Temporal Processor, and send the message to the outbound channel directly
+                // because the message is originated from this node.
+                if let Err(e) = self.outbound_message_sender.send(wrapped_message) {
+                    tracing::error!("Failed to send message to the outbound channel: {e:?}");
+                }
+            }
+            Err(e) => {
+                tracing::error!("Failed to wrap message: {:?}", e);
+            }
+        }
+    }
+
+    fn handle_incoming_message(&mut self, message: Vec<u8>) {
+        match self.cryptographic_processor.unwrap_message(&message) {
+            Ok((unwrapped_message, fully_unwrapped)) => {
+                self.temporal_processor
+                    .push_message(TemporalProcessableMessage {
+                        message: unwrapped_message,
+                        fully_unwrapped,
+                    });
+            }
+            Err(nomos_mix_message::Error::MsgUnwrapNotAllowed) => {
+                tracing::debug!("Message cannot be unwrapped by this node");
+            }
+            Err(e) => {
+                tracing::error!("Failed to unwrap message: {:?}", e);
+            }
+        }
+    }
+
+    fn release_temporal_processed_message(&mut self, message: TemporalProcessableMessage) {
+        if message.fully_unwrapped {
+            if let Err(e) = self.fully_unwrapped_message_sender.send(message.message) {
+                tracing::error!(
+                    "Failed to send fully unwrapped message to the fully unwrapped channel: {e:?}"
+                );
+            }
+        } else if let Err(e) = self.outbound_message_sender.send(message.message) {
+            tracing::error!("Failed to send message to the outbound channel: {e:?}");
+        }
+    }
+}
+
+#[derive(Clone)]
+struct TemporalProcessableMessage {
+    message: Vec<u8>,
+    fully_unwrapped: bool,
 }
