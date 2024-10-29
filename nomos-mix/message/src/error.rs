5162ed2be5d0be6291db94bcf843b3eacd8ceaac
Youngjoon Lee <5462944+youngjoon-lee@users.noreply.github.com>
2024-10-29 23:31:54 +0900

    Mix: Packet header encryption

--- nomos-mix/message/src/error.rs
+++ nomos-mix/message/src/error.rs
@@ -4,10 +4,16 @@ pub enum Error {
     InvalidMixMessage,
     #[error("Payload is too large")]
     PayloadTooLarge,
-    #[error("Too many recipients")]
-    TooManyRecipients,
     #[error("Sphinx packet error: {0}")]
     SphinxPacketError(#[from] sphinx_packet::Error),
+    #[error("Invalid packet")]
+    InvalidPacket,
+    #[error("Invalid routing flag: {0}")]
+    InvalidRoutingFlag(u8),
+    #[error("Invalid routing length: {0}")]
+    InvalidEncryptedRoutingInfoLength(usize),
+    #[error("ConsistentLengthLayeredEncryptionError: {0}")]
+    ConsistentLengthLayeredEncryptionError(#[from] crate::layered_cipher::Error),
     #[error("Unwrapping a message is not allowed to this node")]
     /// e.g. the message cannot be unwrapped using the private key provided
     MsgUnwrapNotAllowed,
