c78ed79201fe39047dc3acf95715a1f9b3de4b82
Roman <zajic@zajic.net>
2024-12-03 18:20:24 +0800

    test: re-enable disseminate_and_retrieve

--- tests/src/tests/da.rs
+++ tests/src/tests/da.rs
@@ -28,7 +28,6 @@ async fn disseminate(executor: &Executor, data: &[u8]) {
     client.publish_blob(data.to_vec(), metadata).await.unwrap();
 }
 
-#[ignore = "todo: reenable after mixnet is tested"]
 #[tokio::test]
 async fn disseminate_and_retrieve() {
     let topology = Topology::spawn(TopologyConfig::validator_and_executor()).await;
@@ -75,7 +74,6 @@ async fn disseminate_and_retrieve() {
     }
 }
 
-//#[ignore = "todo: make work in parallel to other tests"]
 #[tokio::test]
 async fn disseminate_retrieve_reconstruct() {
     let topology = Topology::spawn(TopologyConfig::validator_and_executor()).await;
