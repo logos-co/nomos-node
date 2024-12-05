3278876d16b02b833f844edbc11214744f35afa5
Alejandro Cabeza Romero <alex93cabeza@gmail.com>
2024-12-05 10:54:59 +0100

    Add tracing at mix service level.

--- nomos-services/mix/src/backends/libp2p.rs
+++ nomos-services/mix/src/backends/libp2p.rs
@@ -12,6 +12,8 @@ use libp2p::{
 use nomos_libp2p::{secret_key_serde, DialError, DialOpts};
 use nomos_mix::membership::Membership;
 use nomos_mix_message::sphinx::SphinxMessage;
+use opentelemetry::metrics::{Counter, Histogram};
+use opentelemetry::{global, Key, KeyValue, Value};
 use overwatch_rs::overwatch::handle::OverwatchHandle;
 use rand::Rng;
 use serde::{Deserialize, Serialize};
@@ -20,6 +22,16 @@ use tokio::{
     task::JoinHandle,
 };
 use tokio_stream::wrappers::BroadcastStream;
+
+const SERVICE_NAME: &'static str = "nomos-services/mix/backends/libp2p";
+const METRIC_DATA_RECEIVED: &'static str = "data_received";
+const METRIC_DATA_SENT: &'static str = "data_sent";
+const METRIC_FAILED_INBOUND_MESSAGES: &'static str = "failed_inbound_messages";
+const METRIC_SUCCESSFUL_INBOUND_MESSAGES: &'static str = "successful_inbound_messages";
+const METRIC_FAILED_OUTBOUND_MESSAGES: &'static str = "failed_outbound_messages";
+const METRIC_SUCCESSFUL_OUTBOUND_MESSAGES: &'static str = "successful_outbound_messages";
+const METRIC_ERROR: &'static str = "error";
+const METRIC_IGNORED_EVENT: &'static str = "ignored_event";
 
 /// A mix backend that uses the libp2p network stack.
 pub struct Libp2pMixBackend {
@@ -109,6 +121,16 @@ struct MixSwarm {
     swarm: Swarm<nomos_mix_network::Behaviour<SphinxMessage>>,
     swarm_messages_receiver: mpsc::Receiver<MixSwarmMessage>,
     incoming_message_sender: broadcast::Sender<Vec<u8>>,
+
+    /// Metrics
+    failed_outbound_messages_counter: Counter<u64>,
+    successful_outbound_messages_counter: Counter<u64>,
+    sent_data_histogram: Histogram<u64>,
+    failed_inbound_messages_counter: Counter<u64>,
+    successful_inbound_messages_counter: Counter<u64>,
+    received_data_histogram: Histogram<u64>,
+    error_counter: Counter<u64>,
+    ignored_event_counter: Counter<u64>,
 }
 
 #[derive(Debug)]
@@ -136,10 +158,52 @@ impl MixSwarm {
             })
             .build();
 
+        let meter = global::meter(SERVICE_NAME);
+        let failed_outbound_messages_counter = meter
+            .u64_counter(METRIC_FAILED_OUTBOUND_MESSAGES)
+            .with_description("Number of failed outbound messages")
+            .build();
+        let successful_outbound_messages_counter = meter
+            .u64_counter(METRIC_SUCCESSFUL_OUTBOUND_MESSAGES)
+            .with_description("Number of successful outbound messages")
+            .build();
+        let sent_data_histogram = meter
+            .u64_histogram(METRIC_DATA_SENT)
+            .with_description("Histogram of data sent")
+            .build();
+        let failed_inbound_messages_counter = meter
+            .u64_counter(METRIC_FAILED_INBOUND_MESSAGES)
+            .with_description("Number of failed inbound messages")
+            .build();
+        let successful_inbound_messages_counter = meter
+            .u64_counter(METRIC_SUCCESSFUL_INBOUND_MESSAGES)
+            .with_description("Number of successful inbound messages")
+            .build();
+        let received_data_histogram = meter
+            .u64_histogram(METRIC_DATA_RECEIVED)
+            .with_description("Histogram of data received")
+            .build();
+        let error_counter = meter
+            .u64_counter(METRIC_ERROR)
+            .with_description("Number of errors")
+            .build();
+        let ignored_event_counter = meter
+            .u64_counter(METRIC_IGNORED_EVENT)
+            .with_description("Number of ignored events")
+            .build();
+
         Self {
             swarm,
             swarm_messages_receiver,
             incoming_message_sender,
+            failed_outbound_messages_counter,
+            successful_outbound_messages_counter,
+            sent_data_histogram,
+            failed_inbound_messages_counter,
+            successful_inbound_messages_counter,
+            received_data_histogram,
+            error_counter,
+            ignored_event_counter,
         }
     }
 
@@ -167,8 +231,13 @@ impl MixSwarm {
     async fn handle_swarm_message(&mut self, msg: MixSwarmMessage) {
         match msg {
             MixSwarmMessage::Publish(msg) => {
+                let msg_size = msg.len();
                 if let Err(e) = self.swarm.behaviour_mut().publish(msg) {
                     tracing::error!("Failed to publish message to mix network: {e:?}");
+                    self.failed_outbound_messages_counter.add(1, &[]);
+                } else {
+                    self.successful_outbound_messages_counter.add(1, &[]);
+                    self.sent_data_histogram.record(msg_size as u64, &[]);
                 }
             }
         }
@@ -178,15 +247,23 @@ impl MixSwarm {
         match event {
             SwarmEvent::Behaviour(nomos_mix_network::Event::Message(msg)) => {
                 tracing::debug!("Received message from a peer: {msg:?}");
+
+                let msg_size = msg.len();
                 if let Err(e) = self.incoming_message_sender.send(msg) {
                     tracing::error!("Failed to send incoming message to channel: {e}");
+                    self.failed_inbound_messages_counter.add(1, &[]);
+                } else {
+                    self.successful_inbound_messages_counter.add(1, &[]);
+                    self.received_data_histogram.record(msg_size as u64, &[]);
                 }
             }
             SwarmEvent::Behaviour(nomos_mix_network::Event::Error(e)) => {
                 tracing::error!("Received error from mix network: {e:?}");
+                self.error_counter.add(1, &[]);
             }
             _ => {
                 tracing::debug!("Received event from mix network: {event:?}");
+                self.ignored_event_counter.add(1, &[]);
             }
         }
     }
