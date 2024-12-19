a3bf36d7870efd8470ce0a74e382e8210f801320
Alejandro Cabeza Romero <alex93cabeza@gmail.com>
2024-12-19 14:21:31 +0100

    Update missing error handling on othercrates.

--- nomos-core/chain-defs/src/wire/errors.rs
+++ nomos-core/chain-defs/src/wire/errors.rs
@@ -1,4 +1,5 @@
 //STD
+use crate::wire::bincode::clone_bincode_error;
 use std::io;
 
 #[derive(Debug, thiserror::Error)]
@@ -12,5 +13,14 @@ pub enum Error {
 impl From<Error> for io::Error {
     fn from(value: Error) -> Self {
         io::Error::new(io::ErrorKind::InvalidData, value)
+    }
+}
+
+impl Clone for Error {
+    fn clone(&self) -> Self {
+        match self {
+            Error::Serialize(error) => Error::Serialize(clone_bincode_error(error)),
+            Error::Deserialize(error) => Error::Deserialize(clone_bincode_error(error)),
+        }
     }
 }
--- nomos-da/network/core/src/protocols/mod.rs
+++ nomos-da/network/core/src/protocols/mod.rs
@@ -1,19 +1,3 @@
 pub mod dispersal;
 pub mod replication;
 pub mod sampling;
-use nomos_core::wire;
-
-fn clone_deserialize_error(error: &wire::Error) -> wire::Error {
-    use wire::ErrorKind;
-    Box::new(match error.as_ref() {
-        ErrorKind::Io(error) => ErrorKind::Io(std::io::Error::new(error.kind(), error.to_string())),
-        ErrorKind::InvalidUtf8Encoding(error) => ErrorKind::InvalidUtf8Encoding(*error),
-        ErrorKind::InvalidBoolEncoding(bool) => ErrorKind::InvalidBoolEncoding(*bool),
-        ErrorKind::InvalidCharEncoding => ErrorKind::InvalidCharEncoding,
-        ErrorKind::InvalidTagEncoding(tag) => ErrorKind::InvalidTagEncoding(*tag),
-        ErrorKind::DeserializeAnyNotSupported => ErrorKind::DeserializeAnyNotSupported,
-        ErrorKind::SizeLimit => ErrorKind::SizeLimit,
-        ErrorKind::SequenceMustHaveLength => ErrorKind::SequenceMustHaveLength,
-        ErrorKind::Custom(custom) => ErrorKind::Custom(custom.clone()),
-    })
-}
