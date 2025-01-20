d5442cb99800bbc36c790e90039c30cf8a641a67
Alejandro Cabeza Romero <alex93cabeza@gmail.com>
2025-01-20 16:37:50 +0100

    Add target triple-based target directory parsing.

--- nomos-bundler/src/utils.rs
+++ nomos-bundler/src/utils.rs
@@ -2,6 +2,30 @@
 use std::path::PathBuf;
 use std::process::Command;
 
+pub enum Architecture {
+    X86_64,
+    Aarch64,
+    I686,
+}
+
+impl Architecture {
+    pub fn from_target_triple(target_triple: &str) -> Self {
+        let architecture = target_triple.split("-").next().unwrap();
+        Self::from(architecture)
+    }
+}
+
+impl From<&str> for Architecture {
+    fn from(value: &str) -> Self {
+        match value.to_lowercase().as_str() {
+            "x86_64" => Architecture::X86_64,
+            "aarch64" => Architecture::Aarch64,
+            "i686" => Architecture::I686,
+            _ => panic!("Unsupported architecture"),
+        }
+    }
+}
+
 fn cargo_metadata() -> serde_json::Value {
     let output = Command::new("cargo")
         .arg("metadata")
@@ -21,13 +45,19 @@ pub fn get_target_directory() -> PathBuf {
 }
 
 #[cfg(debug_assertions)]
-pub const fn get_profile() -> &'static str {
-    "debug"
+pub const fn get_profile(architecture: Architecture) -> &'static str {
+    match architecture {
+        Architecture::I686 => "x86_64-unknown-linux-gnu/debug",
+        _ => "debug",
+    }
 }
 
 #[cfg(not(debug_assertions))]
-pub const fn get_profile() -> &'static str {
-    "release"
+pub const fn get_profile(architecture: Architecture) -> &'static str {
+    match architecture {
+        Architecture::i686 => "x86_64-unknown-linux-gnu/release",
+        _ => "release",
+    }
 }
 
 pub fn get_project_identifier(crate_name: &str) -> String {
@@ -42,8 +72,8 @@ pub fn get_workspace_root() -> PathBuf {
     PathBuf::from(workspace_root)
 }
 
-pub fn get_target_directory_for_current_profile() -> PathBuf {
+pub fn get_target_directory_for_current_profile(architecture: Architecture) -> PathBuf {
     let target_directory = get_target_directory();
-    let profile = get_profile();
+    let profile = get_profile(architecture);
     target_directory.join(profile)
 }
