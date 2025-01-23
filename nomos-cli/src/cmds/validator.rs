018df746b9befb95adca9e2c0b20d2fe2b8eab61
Roman <zajic@zajic.net>
2025-01-23 16:05:36 +0800

    chore: add reconstruct command to cli

--- nomos-cli/src/cmds/validator.rs
+++ nomos-cli/src/cmds/validator.rs
@@ -7,6 +7,7 @@ use reqwest::{Client, Url};
 use serde::{de::DeserializeOwned, Serialize};
 // internal
 use kzgrs_backend::{common::blob::DaBlob, dispersal::Index};
+use kzgrs_backend::reconstruction::reconstruct_without_missing_data;
 use nomos_core::da::blob::metadata;
 use nomos_node::api::{handlers::GetRangeReq, paths};
 use nomos_node::wire;
@@ -28,6 +29,14 @@ pub struct Retrieve {
     #[clap(long)]
     pub addr: Url,
 }
+
+#[derive(Args, Debug)]
+pub struct Reconstruct {
+    /// Blobs to use for reconstruction.
+    #[clap(short, long)]
+    pub app_blobs: String,
+}
+
 
 impl Retrieve {
     pub fn run(self) -> Result<(), Box<dyn std::error::Error>> {
@@ -114,4 +123,29 @@ where
         .json::<Vec<(Metadata::Index, Vec<Vec<u8>>)>>()
         .await
         .unwrap())
+}
+
+impl Reconstruct {
+    pub fn run(self) -> Result<(), Box<dyn std::error::Error>> {
+        tracing::subscriber::set_global_default(tracing_subscriber::FmtSubscriber::new())
+            .expect("setting tracing default failed");
+
+        let app_blobs: Vec<(Index, Vec<Vec<u8>>)> = serde_json::from_str(self.app_blobs.as_str())?;
+        
+        let mut da_blobs = vec![];
+        
+        for (index, blobs) in app_blobs.iter() {
+            tracing::info!("Index {:?} has {:} blobs", (index), blobs.len());
+            for blob in blobs.iter() {
+                let da_blob = wire::deserialize::<DaBlob>(blob).unwrap();
+                da_blobs.push(da_blob);
+                tracing::info!("Index {:?}; Blob: {blob:?}", index.to_u64());
+            }
+        }
+
+        let reconstructed_data = reconstruct_without_missing_data(&da_blobs);
+        tracing::info!("Reconstructed data {:?}", reconstructed_data);
+        
+        Ok(())
+    }
 }
