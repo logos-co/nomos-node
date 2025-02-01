b883ebf7241bf58af8739f9f8c2200732d49d2dd
Youngjoon Lee <5462944+youngjoon-lee@users.noreply.github.com>
2025-02-01 16:58:36 +0900

    refactor(blend): enhance connection management structures (#986)
    * refactor(blend): add PeerId to Membership
    
    * refactor(blend): perform conn monitoring in the ConnectionHandler level (#987)
    
    * refactor(blend): drop streams if malicious or on error (#988)
    
    * refactor(blend): establish new connections, or deny new streams (#989)

--- nomos-blend/network/src/lib.rs
+++ nomos-blend/network/src/lib.rs
@@ -2,36 +2,59 @@ mod behaviour;
 mod error;
 mod handler;
 
-pub use behaviour::{Behaviour, Config, Event};
+#[cfg(feature = "tokio")]
+use std::time::Duration;
+
+pub use behaviour::{Behaviour, Config, Event, IntervalStreamProvider};
+use tokio_stream::StreamExt;
+
+#[cfg(feature = "tokio")]
+pub struct TokioIntervalStreamProvider;
+
+#[cfg(feature = "tokio")]
+impl IntervalStreamProvider for TokioIntervalStreamProvider {
+    fn interval_stream(interval: Duration) -> impl futures::Stream<Item = ()> + Send + 'static {
+        // Since tokio::time::interval.tick() returns immediately regardless of the interval,
+        // we need to explicitly specify the time of the first tick we expect.
+        // If not, the peer would be marked as unhealthy immediately
+        // as soon as the connection is established.
+        let start = tokio::time::Instant::now() + interval;
+        tokio_stream::wrappers::IntervalStream::new(tokio::time::interval_at(start, interval))
+            .map(|_| ())
+    }
+}
 
 #[cfg(test)]
+#[cfg(feature = "tokio")]
 mod test {
     use std::time::Duration;
 
     use fixed::types::U57F7;
     use libp2p::{
         futures::StreamExt,
+        identity::Keypair,
         swarm::{dummy, NetworkBehaviour, SwarmEvent},
-        Multiaddr, Swarm, SwarmBuilder,
+        Multiaddr, PeerId, Swarm, SwarmBuilder,
     };
-    use nomos_blend::{
-        conn_maintenance::{ConnectionMaintenanceSettings, ConnectionMonitorSettings},
-        membership::{Membership, Node},
-    };
+    use nomos_blend::{conn_maintenance::ConnectionMonitorSettings, membership::Node};
     use nomos_blend_message::{mock::MockBlendMessage, BlendMessage};
-    use rand::{rngs::ThreadRng, thread_rng};
     use tokio::select;
-    use tokio_stream::wrappers::IntervalStream;
 
-    use crate::{behaviour::Config, error::Error, Behaviour, Event};
+    use crate::{behaviour::Config, error::Error, Behaviour, Event, TokioIntervalStreamProvider};
 
     /// Check that a published messsage arrives in the peers successfully.
     #[tokio::test]
     async fn behaviour() {
         // Initialize two swarms that support the blend protocol.
-        let nodes = nodes(2, 8090);
-        let mut swarm1 = new_swarm(Membership::new(nodes.clone(), nodes[0].public_key));
-        let mut swarm2 = new_swarm(Membership::new(nodes.clone(), nodes[1].public_key));
+        let (mut nodes, mut keypairs) = nodes(2, 8090);
+        let node1_addr = nodes.next().unwrap().address;
+        let mut swarm1 = new_blend_swarm(keypairs.next().unwrap(), node1_addr.clone(), None);
+        let mut swarm2 = new_blend_swarm(
+            keypairs.next().unwrap(),
+            nodes.next().unwrap().address,
+            None,
+        );
+        swarm2.dial(node1_addr).unwrap();
 
         // Swamr2 publishes a message.
         let task = async {
@@ -70,9 +93,15 @@ mod test {
     #[tokio::test]
     async fn peer_not_support_blend_protocol() {
         // Only swarm2 supports the blend protocol.
-        let nodes = nodes(2, 8190);
-        let mut swarm1 = new_swarm_without_blend(nodes[0].address.clone());
-        let mut swarm2 = new_swarm(Membership::new(nodes.clone(), nodes[1].public_key));
+        let (mut nodes, mut keypairs) = nodes(2, 8190);
+        let node1_addr = nodes.next().unwrap().address;
+        let mut swarm1 = new_dummy_swarm(keypairs.next().unwrap(), node1_addr.clone());
+        let mut swarm2 = new_blend_swarm(
+            keypairs.next().unwrap(),
+            nodes.next().unwrap().address,
+            None,
+        );
+        swarm2.dial(node1_addr).unwrap();
 
         // Expect all publish attempts to fail with [`Error::NoPeers`]
         // because swarm2 doesn't have any peers that support the blend protocol.
@@ -94,47 +123,168 @@ mod test {
         }
     }
 
-    fn new_swarm(
-        membership: Membership<MockBlendMessage>,
-    ) -> Swarm<Behaviour<MockBlendMessage, ThreadRng, IntervalStream>> {
-        let conn_maintenance_settings = ConnectionMaintenanceSettings {
-            peering_degree: membership.size() - 1, // excluding the local node
-            max_peering_degree: membership.size() * 2,
-            monitor: Some(ConnectionMonitorSettings {
-                time_window: Duration::from_secs(60),
-                expected_effective_messages: U57F7::from_num(1.0),
-                effective_message_tolerance: U57F7::from_num(0.1),
-                expected_drop_messages: U57F7::from_num(1.0),
-                drop_message_tolerance: U57F7::from_num(0.1),
-            }),
+    #[tokio::test]
+    async fn detect_malicious_peer() {
+        // Init two swarms with connection monitoring enabled.
+        let conn_monitor_settings = ConnectionMonitorSettings {
+            interval: Duration::from_secs(1),
+            expected_effective_messages: U57F7::from_num(0.0),
+            effective_message_tolerance: U57F7::from_num(0.0),
+            expected_drop_messages: U57F7::from_num(0.0),
+            drop_message_tolerance: U57F7::from_num(0.0),
         };
-        let conn_maintenance_interval = conn_maintenance_settings
-            .monitor
-            .as_ref()
-            .map(|monitor| IntervalStream::new(tokio::time::interval(monitor.time_window)));
-        let mut swarm = new_swarm_with_behaviour(Behaviour::new(
-            Config {
+        let (mut nodes, mut keypairs) = nodes(2, 8290);
+        let node1_addr = nodes.next().unwrap().address;
+        let mut swarm1 = new_blend_swarm(
+            keypairs.next().unwrap(),
+            node1_addr.clone(),
+            Some(conn_monitor_settings),
+        );
+        let mut swarm2 = new_blend_swarm(
+            keypairs.next().unwrap(),
+            nodes.next().unwrap().address,
+            Some(conn_monitor_settings),
+        );
+        swarm2.dial(node1_addr).unwrap();
+
+        // Swarm2 sends a message to Swarm1, even though expected_effective_messages is 0.
+        // Then, Swarm1 should detect Swarm2 as a malicious peer.
+        let task = async {
+            let mut num_events_waiting = 2;
+            let mut msg_published = false;
+            let mut publish_try_interval = tokio::time::interval(Duration::from_millis(10));
+            loop {
+                if num_events_waiting == 0 {
+                    break;
+                }
+
+                select! {
+                    _ = publish_try_interval.tick() => {
+                        if !msg_published {
+                            msg_published = swarm2.behaviour_mut().publish(vec![1; 10]).is_ok();
+                        }
+                    }
+                    event = swarm1.select_next_some() => {
+                        match event {
+                            // We expect the behaviour reports a malicious peer.
+                            SwarmEvent::Behaviour(Event::MaliciousPeer(peer_id)) => {
+                                assert_eq!(peer_id, *swarm2.local_peer_id());
+                                num_events_waiting -= 1;
+                            },
+                            // We expect that the Swarm1 closes the connection proactively.
+                            SwarmEvent::ConnectionClosed { peer_id, num_established, .. } => {
+                                assert_eq!(peer_id, *swarm2.local_peer_id());
+                                assert_eq!(num_established, 0);
+                                assert!(swarm1.connected_peers().next().is_none());
+                                num_events_waiting -= 1;
+                            },
+                            _ => {},
+                        }
+                    }
+                    _ = swarm2.select_next_some() => {}
+                }
+            }
+        };
+
+        // Expect for the task to be completed in time
+        assert!(tokio::time::timeout(
+            conn_monitor_settings.interval + Duration::from_secs(1),
+            task
+        )
+        .await
+        .is_ok());
+    }
+
+    #[tokio::test]
+    async fn detect_unhealthy_peer() {
+        // Init two swarms with connection monitoring enabled.
+        let conn_monitor_settings = ConnectionMonitorSettings {
+            interval: Duration::from_secs(1),
+            expected_effective_messages: U57F7::from_num(1.0),
+            effective_message_tolerance: U57F7::from_num(0.0),
+            expected_drop_messages: U57F7::from_num(0.0),
+            drop_message_tolerance: U57F7::from_num(0.0),
+        };
+        let (mut nodes, mut keypairs) = nodes(2, 8390);
+        let node1_addr = nodes.next().unwrap().address;
+        let mut swarm1 = new_blend_swarm(
+            keypairs.next().unwrap(),
+            node1_addr.clone(),
+            Some(conn_monitor_settings),
+        );
+        let mut swarm2 = new_blend_swarm(
+            keypairs.next().unwrap(),
+            nodes.next().unwrap().address,
+            Some(conn_monitor_settings),
+        );
+        swarm2.dial(node1_addr).unwrap();
+
+        // Swarms don't send anything, even though expected_effective_messages is 1.
+        // Then, both should detect the other as unhealthy.
+        // Swarms shouldn't close the connection of the unhealthy peers.
+        let task = async {
+            let mut num_events_waiting = 2;
+            loop {
+                if num_events_waiting == 0 {
+                    break;
+                }
+
+                select! {
+                    event = swarm1.select_next_some() => {
+                        if let SwarmEvent::Behaviour(Event::UnhealthyPeer(peer_id)) = event {
+                            assert_eq!(peer_id, *swarm2.local_peer_id());
+                            num_events_waiting -= 1;
+                        }
+                    }
+                    event = swarm2.select_next_some() => {
+                        if let SwarmEvent::Behaviour(Event::UnhealthyPeer(peer_id)) = event {
+                            assert_eq!(peer_id, *swarm1.local_peer_id());
+                            num_events_waiting -= 1;
+                        }
+                    }
+                }
+            }
+
+            assert_eq!(swarm1.behaviour().num_healthy_peers(), 0);
+            assert_eq!(swarm1.connected_peers().count(), 1);
+            assert_eq!(swarm2.behaviour().num_healthy_peers(), 0);
+            assert_eq!(swarm2.connected_peers().count(), 1);
+        };
+
+        // Expect for the task to be completed in time
+        assert!(tokio::time::timeout(
+            conn_monitor_settings.interval + Duration::from_secs(1),
+            task
+        )
+        .await
+        .is_ok());
+    }
+
+    fn new_blend_swarm(
+        keypair: Keypair,
+        addr: Multiaddr,
+        conn_monitor_settings: Option<ConnectionMonitorSettings>,
+    ) -> Swarm<Behaviour<MockBlendMessage, TokioIntervalStreamProvider>> {
+        new_swarm_with_behaviour(
+            keypair,
+            addr,
+            Behaviour::<MockBlendMessage, TokioIntervalStreamProvider>::new(Config {
                 duplicate_cache_lifespan: 60,
-                conn_maintenance_settings,
-                conn_maintenance_interval,
-            },
-            membership.clone(),
-            thread_rng(),
-        ));
-        swarm
-            .listen_on(membership.local_node().address.clone())
-            .unwrap();
-        swarm
+                conn_monitor_settings,
+            }),
+        )
     }
 
-    fn new_swarm_without_blend(addr: Multiaddr) -> Swarm<dummy::Behaviour> {
-        let mut swarm = new_swarm_with_behaviour(dummy::Behaviour);
-        swarm.listen_on(addr).unwrap();
-        swarm
+    fn new_dummy_swarm(keypair: Keypair, addr: Multiaddr) -> Swarm<dummy::Behaviour> {
+        new_swarm_with_behaviour(keypair, addr, dummy::Behaviour)
     }
 
-    fn new_swarm_with_behaviour<B: NetworkBehaviour>(behaviour: B) -> Swarm<B> {
-        SwarmBuilder::with_new_identity()
+    fn new_swarm_with_behaviour<B: NetworkBehaviour>(
+        keypair: Keypair,
+        addr: Multiaddr,
+        behaviour: B,
+    ) -> Swarm<B> {
+        let mut swarm = SwarmBuilder::with_existing_identity(keypair)
             .with_tokio()
             .with_other_transport(|keypair| {
                 libp2p::quic::tokio::Transport::new(libp2p::quic::Config::new(keypair))
@@ -142,23 +292,39 @@ mod test {
             .unwrap()
             .with_behaviour(|_| behaviour)
             .unwrap()
-            .with_swarm_config(|cfg| {
-                cfg.with_idle_connection_timeout(std::time::Duration::from_secs(u64::MAX))
+            .with_swarm_config(|config| {
+                // We want connections to be closed immediately as soon as
+                // the corresponding streams are dropped by behaviours.
+                config.with_idle_connection_timeout(Duration::ZERO)
             })
-            .build()
+            .build();
+        swarm.listen_on(addr).unwrap();
+        swarm
     }
 
     fn nodes(
         count: usize,
         base_port: usize,
-    ) -> Vec<Node<<MockBlendMessage as BlendMessage>::PublicKey>> {
-        (0..count)
-            .map(|i| Node {
+    ) -> (
+        impl Iterator<Item = Node<PeerId, <MockBlendMessage as BlendMessage>::PublicKey>>,
+        impl Iterator<Item = Keypair>,
+    ) {
+        let mut nodes = Vec::with_capacity(count);
+        let mut keypairs = Vec::with_capacity(count);
+
+        for i in 0..count {
+            let keypair = Keypair::generate_ed25519();
+            let node = Node {
+                id: PeerId::from(keypair.public()),
                 address: format!("/ip4/127.0.0.1/udp/{}/quic-v1", base_port + i)
                     .parse()
                     .unwrap(),
                 public_key: [i as u8; 32],
-            })
-            .collect()
+            };
+            nodes.push(node);
+            keypairs.push(keypair);
+        }
+
+        (nodes.into_iter(), keypairs.into_iter())
     }
 }
