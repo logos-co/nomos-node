9020d39b4b13c6a2537e7201e943522596680a6a
Alejandro Cabeza Romero <alex93cabeza@gmail.com>
2025-02-05 13:12:29 +0100

    Add optimize todo.

--- nomos-services/cryptarchia-consensus/src/lib.rs
+++ nomos-services/cryptarchia-consensus/src/lib.rs
@@ -964,6 +964,7 @@ where
         security_param: &u32,
         storage_adapter: &StorageAdapter<Storage, TxS::Tx, BS::BlobId>,
     ) -> Option<Block<ClPool::Item, DaPool::Item>> {
+        // TODO: This implies fetching from DB `security_param` times. We should optimize this.
         let mut current_block = block;
         for _ in 0..*security_param {
             let parent_block_header = current_block.header().parent();
