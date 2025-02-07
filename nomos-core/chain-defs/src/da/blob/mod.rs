3701cff87e29027e91c9d211b4287f5ac1449a6e
Alejandro Cabeza Romero <alex93cabeza@gmail.com>
2025-02-07 14:21:56 +0100

    Move constraints to a higher layer.

--- nomos-core/chain-defs/src/da/blob/mod.rs
+++ nomos-core/chain-defs/src/da/blob/mod.rs
@@ -12,7 +12,7 @@ pub trait Blob {
 
 pub trait BlobSelect {
     type BlobId: info::DispersedBlobInfo;
-    type Settings: Clone + Send;
+    type Settings: Clone;
 
     fn new(settings: Self::Settings) -> Self;
     fn select_blob_from<'i, I: Iterator<Item = Self::BlobId> + 'i>(
