0ce60d78bc506df918bac90c8e78dc435e947a67
danielSanchezQ <3danimanimal@gmail.com>
2025-02-11 11:13:40 +0000

    Create EpochConfig

--- ledger/nomos-ledger/src/config.rs
+++ ledger/nomos-ledger/src/config.rs
@@ -3,16 +3,7 @@ use cryptarchia_engine::{Epoch, Slot};
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[derive(Clone, Debug, PartialEq)]
 pub struct Config {
-    // The stake distribution is always taken at the beginning of the previous epoch.
-    // This parameters controls how many slots to wait for it to be stabilized
-    // The value is computed as epoch_stake_distribution_stabilization * int(floor(k / f))
-    pub epoch_stake_distribution_stabilization: u8,
-    // This parameter controls how many slots we wait after the stake distribution
-    // snapshot has stabilized to take the nonce snapshot.
-    pub epoch_period_nonce_buffer: u8,
-    // This parameter controls how many slots we wait for the nonce snapshot to be considered
-    // stabilized
-    pub epoch_period_nonce_stabilization: u8,
+    pub epoch_config: cryptarchia_engine::EpochConfig,
     pub consensus_config: cryptarchia_engine::Config,
 }
 
@@ -22,15 +13,14 @@ impl Config {
     }
 
     pub fn epoch_length(&self) -> u64 {
-        (self.epoch_stake_distribution_stabilization as u64
-            + self.epoch_period_nonce_buffer as u64
-            + self.epoch_period_nonce_stabilization as u64)
-            * self.base_period_length()
+        self.epoch_config
+            .epoch_length(self.consensus_config.base_period_length())
     }
 
     pub fn nonce_snapshot(&self, epoch: Epoch) -> Slot {
         let offset = self.base_period_length()
-            * (self.epoch_period_nonce_buffer + self.epoch_stake_distribution_stabilization) as u64;
+            * (self.epoch_config.epoch_period_nonce_buffer
+                + self.epoch_config.epoch_stake_distribution_stabilization) as u64;
         let base = (u32::from(epoch) - 1) as u64 * self.epoch_length();
         (base + offset).into()
     }
@@ -40,7 +30,8 @@ impl Config {
     }
 
     pub fn epoch(&self, slot: Slot) -> Epoch {
-        ((u64::from(slot) / self.epoch_length()) as u32).into()
+        self.epoch_config
+            .epoch(slot, self.consensus_config.base_period_length())
     }
 }
 
