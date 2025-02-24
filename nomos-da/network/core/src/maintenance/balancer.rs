a721a0dfa360612bcbcf763f1c2fb2872395b176
Gusto <bacvinka@gmail.com>
2025-02-24 17:02:06 +0200

    Remove swam dial calls from da network service

--- nomos-da/network/core/src/maintenance/balancer.rs
+++ nomos-da/network/core/src/maintenance/balancer.rs
@@ -2,7 +2,7 @@
 use std::{
     collections::VecDeque,
     convert::Infallible,
-    task::{Context, Poll},
+    task::{Context, Poll, Waker},
 };
 // crates
 use libp2p::{
@@ -36,19 +36,31 @@ pub struct ConnectionBalancerBehaviour<Balancer> {
     addresses: AddressBook,
     balancer: Balancer,
     peers_to_dial: VecDeque<PeerId>,
+    waker: Option<Waker>,
 }
 
-impl<Balancer> ConnectionBalancerBehaviour<Balancer> {
+impl<Balancer> ConnectionBalancerBehaviour<Balancer>
+where
+    Balancer: ConnectionBalancer,
+{
     pub fn new(addresses: AddressBook, balancer: Balancer) -> Self {
         Self {
             addresses,
             balancer,
             peers_to_dial: VecDeque::new(),
+            waker: None,
         }
     }
 
     pub fn update_addresses(&mut self, addresses: AddressBook) {
         self.addresses = addresses;
+    }
+
+    fn record_event(&mut self, event: ConnectionEvent) {
+        self.balancer.record_event(event);
+        if let Some(waker) = self.waker.take() {
+            waker.wake();
+        }
     }
 }
 
@@ -66,8 +78,7 @@ where
         _: &Multiaddr,
         _: &Multiaddr,
     ) -> Result<Self::ConnectionHandler, ConnectionDenied> {
-        self.balancer
-            .record_event(ConnectionEvent::OpenInbound(peer));
+        self.record_event(ConnectionEvent::OpenInbound(peer));
         Ok(dummy::ConnectionHandler)
     }
 
@@ -79,8 +90,7 @@ where
         _: Endpoint,
         _: PortUse,
     ) -> Result<Self::ConnectionHandler, ConnectionDenied> {
-        self.balancer
-            .record_event(ConnectionEvent::OpenOutbound(peer));
+        self.record_event(ConnectionEvent::OpenOutbound(peer));
         Ok(dummy::ConnectionHandler)
     }
 
@@ -91,12 +101,10 @@ where
         {
             match endpoint {
                 Dialer { .. } => {
-                    self.balancer
-                        .record_event(ConnectionEvent::CloseInbound(peer_id));
+                    self.record_event(ConnectionEvent::CloseInbound(peer_id));
                 }
                 Listener { .. } => {
-                    self.balancer
-                        .record_event(ConnectionEvent::CloseOutbound(peer_id));
+                    self.record_event(ConnectionEvent::CloseOutbound(peer_id));
                 }
             }
         }
@@ -131,6 +139,7 @@ where
             }
         }
 
+        self.waker = Some(cx.waker().clone());
         Poll::Pending
     }
 }
