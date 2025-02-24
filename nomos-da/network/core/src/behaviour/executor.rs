56bc97e0d2cf4663b0a4a7b7d7d42099c9b9a1bf
Gusto <bacvinka@gmail.com>
2025-02-24 17:02:06 +0200

    Remove swam dial calls from da network service

--- nomos-da/network/core/src/behaviour/executor.rs
+++ nomos-da/network/core/src/behaviour/executor.rs
@@ -60,7 +60,6 @@ where
         Self {
             sampling: SamplingBehaviour::new(peer_id, membership.clone(), addresses.clone()),
             executor_dispersal: DispersalExecutorBehaviour::new(
-                peer_id,
                 membership.clone(),
                 addresses.clone(),
             ),
