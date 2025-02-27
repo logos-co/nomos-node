11769f4b84a1879f291ad5ab3e2cf1cf21d2524b
Andrus Salumets <andrus@lagrange.dev>
2025-02-27 12:05:34 +0100

    feat(api): get shared commitments

--- nomos-services/api/src/http/storage.rs
+++ nomos-services/api/src/http/storage.rs
@@ -1,6 +1,8 @@
 use nomos_core::da::blob::Blob;
 use nomos_da_storage::rocksdb::{key_bytes, DA_SHARED_COMMITMENTS_PREFIX};
 use nomos_core::{block::Block, header::HeaderId};
+use nomos_core::da::blob::Blob;
+use nomos_da_storage::rocksdb::{key_bytes, DA_SHARED_COMMITMENTS_PREFIX};
 use nomos_storage::{
     backends::{rocksdb::RocksBackend, StorageSerde},
     StorageMsg, StorageService,
